92b659b187b49c9c67293c4fdfa43feb822318bc
Stephen Dorn <steved11@sbcglobal.net>
2017-11-30 19:19:31 -0600

    fixed top menu to work with material-ui

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,28 @@
 "use strict"
 import React, { Component } from 'react';
-import {AppBar} from 'material-ui';
-import FlatButton from 'material-ui/FlatButton';
+import {AppBar, FlatButton, ToolbarGroup } from 'material-ui';
 
 class Menu extends Component {
+
     render() {
+        const styles = {
+            title: {
+                cursor: 'pointer',
+            },
+        };
+        const myButtons = (
+            <div>
+                <FlatButton label="Login" href="/login" hoverColor='#00c3ff' secondary={true}/>
+                <FlatButton label="Signup" href="/signup" hoverColor='#00c3ff' secondary={true}/>
+            </div>
+        );
+
         return (
-            <AppBar title="Moonshot Learning" iconElementRight={<FlatButton href="/admin" label="Admin" />}>
+            <AppBar title={<span style={styles.title}>Moonshot Learning</span>}
+                    iconElementRight={<ToolbarGroup>{myButtons}</ToolbarGroup>}
+            >
             </AppBar>
+
         )
     }
 }
