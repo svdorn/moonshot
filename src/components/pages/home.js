d407c9618d4d8b986d69c372db28571a92aeb3ab
Stephen Dorn <steved11@sbcglobal.net>
2017-12-01 11:47:46 -0600

    added styling to homepage and forms

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,17 @@
 "use strict"
 import React, { Component } from 'react';
-
+import { Paper } from 'material-ui';
 class Home extends Component{
+
     render(){
         return(
           <div>
-              Home
+              <Paper className="form" zDepth={2}>
+                  <h1>Home</h1>
+                  <h3>
+                      Moonshot learning is a company that is amazing.
+                  </h3>
+              </Paper>
           </div>
         );
     }
