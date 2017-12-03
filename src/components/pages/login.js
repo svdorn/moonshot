879da524aa5ac8fb72632611df46454a0ab879ea
Stephen Dorn <steved11@sbcglobal.net>
2017-12-03 12:58:06 -0600

    changed login form to have validation checking with redux forms

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,10 +1,10 @@
 "use strict"
-import React, { Component } from 'react';
-import { TextField, RaisedButton, Paper } from 'material-ui';
-import { login } from '../../actions/usersActions';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
+import React, {Component} from 'react';
+import {TextField, RaisedButton, Paper} from 'material-ui';
+import {login} from '../../actions/usersActions';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { Field, reduxForm } from 'redux-form';
 
 
 const styles = {
@@ -14,55 +14,92 @@ const styles = {
 
 };
 
+const renderTextField = ({input, label, meta: {touched, error}, ...custom}) => (
+    <TextField
+        hintText={label}
+        floatingLabelText={label}
+        errorText={touched && error}
+        floatingLabelStyle={styles.floatingLabelStyle}
+        {...input}
+        {...custom}
+    />
+);
+
+const validate = values => {
+    const errors = {};
+    const requiredFields = [
+        'username',
+        'password',
+    ];
+    requiredFields.forEach(field => {
+        if (!values[field]) {
+            errors[field] = 'This field is required'
+        }
+    });
+
+    return errors
+};
+
 class Login extends Component {
 
-    handleSubmit(){
+    handleSubmit() {
+        console.log("username is " + this.props.formData.login.values.username);
         const user = {
-            username: this.refs.username.getValue(),
-            password: this.refs.password.getValue()
+            username: this.props.formData.login.values.username,
+            password: this.props.formData.login.values.password
         };
 
         console.log(user);
 
         this.props.login(user);
+        console.log("here");
+        console.log("current user is" + this.props.currentUser);
 
         //console.log(this.state.currentUser);
     }
 
-    render(){
+    render() {
+        console.log(this.props);
         return (
             <Paper className="form" zDepth={2}>
-                <h1>Login</h1>
-                <TextField
-                    floatingLabelText="Username or Email"
-                    type="text"
-                    floatingLabelStyle={styles.floatingLabelStyle}
-                    ref="username"
-                /><br />
-                <TextField
-                    required={true}
-                    valueType={String}
-                    floatingLabelText="Password"
-                    type="password"
-                    floatingLabelStyle={styles.floatingLabelStyle}
-                    ref="password"
-                /><br />
-                <RaisedButton onClick={this.handleSubmit.bind(this)} type="submit" label="Login" primary={true} className="button" />
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <h1>Login</h1>
+                        <Field
+                            name="username"
+                            component={renderTextField}
+                            label="Username"
+                        /><br/>
+                        <Field
+                            name="password"
+                            component={renderTextField}
+                            label="Password"
+                        /><br/>
+                    <RaisedButton type="submit"
+                                  label="Login"
+                                  primary={true}
+                                  className="button"/>
+                </form>
             </Paper>
         );
     }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    login
-  }, dispatch);
+    return bindActionCreators({
+        login
+    }, dispatch);
 }
 
 function mapStateToProps(state) {
-  return {
-    currentUser: state.currentUser
-  };
+    return {
+        currentUser: state.currentUser,
+        formData: state.form
+    };
 }
+
+Login = reduxForm({
+    form:'login',
+    validate,
+})(Login);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/reducers/usersReducers.js
+++ src/reducers/usersReducers.js
@@ -1,72 +1,84 @@
 "use strict"
 
 // USERS REDUCERS
-export function usersReducers(state={users: []}, action) {
-  switch(action.type) {
-    case "GET_USERS":
-    return {...state, users:[...action.payload]}
-    break;
-    case "LOGIN":
-    return {...state, currentUser:action.payload}
-    break;
-    case "LOGIN_REJECTED":
-    // TODO deal with failed login
-    console.log("LOGIN FAILED");
-    console.log(action.payload);
-    break;
-    case "POST_USER":
-    return {...state,
-        users: [...state.users, ...action.payload],
-        msg: 'Saved! Click to continue',
-        style: 'success',
-        validation: 'success'}
-    break;
-    case "POST_USER_REJECTED":
-    return {...state,
-        msg:'Please try again',
-        style:'danger',
-        validation: 'error'}
-    break;
-    case "RESET_BUTTON":
-    return {...state,
-        msg: null,
-        style: 'primary',
-        validation: null};
-    break;
-    case "DELETE_USER":
-    // Create a copy of the current array of users
-    const currentUserToDelete = [...state.users];
-    // Determine at which index in users array is
-    // the user to be deleted
-    const indexToDelete = currentUserToDelete.findIndex(
-      function(user) {
-        return user._id == action.payload;
-      }
-    )
-    // use slice to remove the user at the specified index
-    return {users: [...currentUserToDelete.slice(0, indexToDelete),
-    ...currentUserToDelete.slice(indexToDelete + 1)]}
-    break;
+export function usersReducers(state = {users: []}, action) {
+    switch (action.type) {
+        case "GET_USERS":
+            return {...state, users: [...action.payload]}
+            break;
+        case "LOGIN":
+            console.log("printing payload");
+            console.log(action.payload);
+            return {...state, currentUser: action.payload}
+            break;
+        case "LOGIN_REJECTED":
+            // TODO deal with failed login
+            console.log("LOGIN FAILED");
+            console.log(action.payload);
+            break;
+        case "POST_USER":
+            return {
+                ...state,
+                users: [...state.users, ...action.payload],
+                msg: 'Saved! Click to continue',
+                style: 'success',
+                validation: 'success'
+            }
+            break;
+        case "POST_USER_REJECTED":
+            return {
+                ...state,
+                msg: 'Please try again',
+                style: 'danger',
+                validation: 'error'
+            }
+            break;
+        case "RESET_BUTTON":
+            return {
+                ...state,
+                msg: null,
+                style: 'primary',
+                validation: null
+            };
+            break;
+        case "DELETE_USER":
+            // Create a copy of the current array of users
+            const currentUserToDelete = [...state.users];
+            // Determine at which index in users array is
+            // the user to be deleted
+            const indexToDelete = currentUserToDelete.findIndex(
+                function (user) {
+                    return user._id == action.payload;
+                }
+            )
+            // use slice to remove the user at the specified index
+            return {
+                users: [...currentUserToDelete.slice(0, indexToDelete),
+                    ...currentUserToDelete.slice(indexToDelete + 1)]
+            }
+            break;
 
-    case "UPDATE_USER":
-    // Create a copy of the current array of users
-    const currentUserToUpdate = [...state.users];
-    // Determine at which index in users array is the user to be deleted
-    const indexToUpdate = currentUserToUpdate.findIndex(
-      function(user) {
-        return user._id === action.payload._id;
-      }
-    )
-    // create a new user object with the new values
-    const newUser = {
-      ...currentUserToUpdate[indexToUpdate],
-      username: action.payload.username
+        case "UPDATE_USER":
+            // Create a copy of the current array of users
+            const currentUserToUpdate = [...state.users];
+            // Determine at which index in users array is the user to be deleted
+            const indexToUpdate = currentUserToUpdate.findIndex(
+                function (user) {
+                    return user._id === action.payload._id;
+                }
+            )
+            // create a new user object with the new values
+            const newUser = {
+                ...currentUserToUpdate[indexToUpdate],
+                username: action.payload.username
+            }
+            console.log("what newUser is: ", newUser);
+            return {
+                users: [...currentUserToUpdate.slice(0, indexToUpdate),
+                    newUser, ...currentUserToUpdate.slice(indexToUpdate + 1)]
+            }
+            break;
     }
-    console.log("what newUser is: ", newUser);
-    return {users: [...currentUserToUpdate.slice(0, indexToUpdate),
-    newUser, ...currentUserToUpdate.slice(indexToUpdate + 1)]}
-    break;
-  }
 
-  return state
+    return state
 }
