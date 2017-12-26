a89ca6677acba72684a0c8f7814122af55669dc8
Austin Meyer <frizzkitten@gmail.com>
2017-12-26 09:45:41 -0600

    Trajectory page done except for the triangles

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -92,8 +92,73 @@ class Home extends Component{
                     <img
                         src="/images/TheMoonshotTrajectory.png"
                         alt="Pathways"
-                        style={{marginTop:"100px", marginBottom:"15px", width:"700px"}}
+                        style={{marginTop:"45px", marginBottom:"15px", width:"700px"}}
                     /><br/>
+                    <div style={{
+                        float:"left",
+                        textAlign:"left",
+                        position:"relative",
+                        marginLeft:"100px"
+                    }}>
+                        <img
+                            src="/icons/Lightbulb.png"
+                            alt="Lightbulb"
+                            style={{height:"90px", position:"absolute", top:"50%", marginTop:"-45px"}}
+                        />
+                        <div className="smallText2" style={{float:"right", marginLeft:"140px"}}>
+                            <h2 className="greenText" style={{fontSize:"32px"}}>
+                                Complete Pathways<br/>
+                                And Learn Skills<br/>
+                            </h2>
+                            We work directly with the most<br/>
+                            innovative companies to curate<br/>
+                            pathways that consist of courses to<br/>
+                            teach you the skills that they look for.
+                        </div>
+                    </div>
+                    <div style={{
+                        float:"right",
+                        textAlign:"left",
+                        position:"relative",
+                        marginRight:"100px",
+                        clear:"both"
+                    }}>
+                        <img
+                            src="/icons/Person.png"
+                            alt="Person"
+                            style={{height:"90px", position:"absolute", top:"50%", marginTop:"-45px"}}
+                        />
+                        <div className="smallText2" style={{float:"right", marginLeft:"140px"}}>
+                            <h2 style={{fontSize:"32px", color:"#2ab6ff"}}>
+                                Build Your Profile<br/>
+                            </h2>
+                            Earn badges by completing pathways.<br/>
+                            Add them to your profile to make yourself<br/>
+                            more attractive to employers.<br/>
+                        </div>
+                    </div>
+                    <div style={{
+                        float:"left",
+                        textAlign:"left",
+                        position:"relative",
+                        marginLeft:"100px",
+                        clear:"both"
+                    }}>
+                        <img
+                            src="/icons/Badge.png"
+                            alt="Badge"
+                            style={{height:"90px", position:"absolute", top:"50%", marginTop:"-45px"}}
+                        />
+                        <div className="smallText2" style={{float:"right", marginLeft:"140px"}}>
+                            <h2 style={{fontSize:"32px", color:"#b25fff"}}>
+                                Get Hired By The Most<br/>
+                                Innovative Companies<br/>
+                            </h2>
+                            Get hired for full-time, part-time,<br/>
+                            internship and contract positions by<br/>
+                            the most forward-thinking companies.
+                        </div>
+                    </div>
                 </div>
 
             </div>
