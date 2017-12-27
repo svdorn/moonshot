96d9df7cdc894531c1f1cb0172c57194f1767457
Austin Meyer <frizzkitten@gmail.com>
2017-12-27 12:46:54 -0600

    partial footer redesign

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,45 @@ import React, { Component } from 'react';
 import {Toolbar, ToolbarTitle, FlatButton, ToolbarGroup } from 'material-ui';
 
 class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer text-center" style={{minWidth:"800px"}}>
-        <Toolbar style={{textAlign:'center'}}>
-            <ToolbarTitle text="Copyright 2017 Moonshot Learning Inc. All rights reserved."/>
-        </Toolbar>
-      </footer>
-    );
-  }
+
+
+    // <Toolbar style={{textAlign:'center'}}>
+    //     <ToolbarTitle
+    //         text="Copyright 2017 Moonshot Learning Inc. All rights reserved."
+    //         style={{
+    //             textAlign:"center",
+    //             fontSize:"12px"
+    //         }}
+    //     />
+    // </Toolbar>
+
+
+    // <iframe
+    //     src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FMoonshotLearning%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId=93815497157"
+    //     width="450"
+    //     height="80"
+    //     style={{border:"none", overflow:"hidden"}}
+    //     scrolling="no"
+    //     frameBorder="0"
+    //     allowTransparency="true">
+    // </iframe>
+    //
+    // <script src="//platform.linkedin.com/in.js" type="text/javascript">
+    //     lang: en_US
+    // </script>
+    // <script type="IN/FollowCompany" data-id="18233111" data-counter="top"></script>
+
+
+
+    render() {
+        return (
+            <div className="jsxWrapper">
+                <footer className="footer text-center" style={{minWidth:"800px"}}>
+                    Copyright 2017 Moonshot Learning Inc. All rights reserved.
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default Footer;
