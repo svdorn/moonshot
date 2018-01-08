137aa303f436c119441676c49579ec1ca95a7b58
Stephen Dorn <steved11@sbcglobal.net>
2018-01-07 22:39:09 -0600

    finished the box on pathways content page

--- src/components/pages/pathwayContent.js
+++ src/components/pages/pathwayContent.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import PathwayContentLink from '../childComponents/pathwayContentLink';
 import PathwayContentVideo from '../childComponents/pathwayContentVideo';
 import PathwayContentArticle from '../childComponents/pathwayContentArticle';
-import {AppBar, Paper} from 'material-ui';
+import {Tabs, Tab, CircularProgress, Paper} from 'material-ui';
 import {connect} from 'react-redux';
 import {browserHistory} from 'react-router';
 import {closeNotification, updateCurrentSubStep} from "../../actions/usersActions";
@@ -36,7 +36,7 @@ class PathwayContent extends Component {
             // if we don't know what step we're currently on
             if (this.props.step == undefined) {
                 // find the current pathway in the user's profile
-                let userPath = this.props.currentUser.pathways.find(function(path) {
+                let userPath = this.props.currentUser.pathways.find(function (path) {
                     return path.pathwayId == pathwayId;
                 });
 
@@ -46,9 +46,9 @@ class PathwayContent extends Component {
                 // the step is set the first step
                 if (userPath.currentStep.step == undefined) {
                     const stepNumber = 1;
-                    const subStep = pathway.steps.find(function(step) {
+                    const subStep = pathway.steps.find(function (step) {
                         return step.order == 1;
-                    }).subSteps.find(function(subStep) {
+                    }).subSteps.find(function (subStep) {
                         return subStep.order == 1;
                     });
                     this.props.updateCurrentSubStep(user, pathwayId, stepNumber, subStep);
@@ -56,9 +56,9 @@ class PathwayContent extends Component {
                 // otherwise save the step that was saved in the db to redux state
                 else {
                     const stepNumber = userPath.currentStep.step;
-                    const subStep = pathway.steps.find(function(step) {
+                    const subStep = pathway.steps.find(function (step) {
                         return step.order == stepNumber;
-                    }).subSteps.find(function(subStep) {
+                    }).subSteps.find(function (subStep) {
                         return subStep.order == userPath.currentStep.subStep;
                     })
                     this.props.updateCurrentSubStep(user, pathwayId, stepNumber, subStep);
@@ -74,13 +74,13 @@ class PathwayContent extends Component {
                 // if the currently saved step is not for the right pathway
                 if (this.props.step.pathwayId != pathwayId) {
                     const user = this.props.currentUser;
-                    let userPath = this.props.currentUser.pathways.find(function(path) {
+                    let userPath = this.props.currentUser.pathways.find(function (path) {
                         return path.pathwayId == pathwayId;
                     });
                     const stepNumber = userPath.currentStep.step;
-                    const subStep = pathway.steps.find(function(step) {
+                    const subStep = pathway.steps.find(function (step) {
                         return step.order == stepNumber;
-                    }).subSteps.find(function(subStep) {
+                    }).subSteps.find(function (subStep) {
                         return subStep.order == userPath.currentStep.subStep;
                     })
                     this.props.updateCurrentSubStep(user, pathwayId, stepNumber, subStep);
@@ -93,7 +93,7 @@ class PathwayContent extends Component {
         // })
     }
 
-    goTo (route)  {
+    goTo(route) {
         // closes any notification
         this.props.closeNotification();
         // goes to the wanted page
@@ -137,6 +137,11 @@ class PathwayContent extends Component {
             },
             contentContainer: {
                 overflow: "auto"
+            },
+            tabs: {},
+            tab: {
+                backgroundColor: "white",
+                color: '#B869FF',
             }
         }
 
@@ -154,7 +159,7 @@ class PathwayContent extends Component {
             if (contentType == "link") {
                 content = <PathwayContentLink style={style.content}/>;
             } else if (contentType == "video") {
-                content = <PathwayContentVideo className="videoContainer" />;
+                content = <PathwayContentVideo className="videoContainer"/>;
             } else if (contentType == "article") {
                 content = <PathwayContentArticle style={style.content}/>
             } else {
@@ -169,35 +174,35 @@ class PathwayContent extends Component {
         }
 
         return (
-            <div style={{marginBottom:"50px"}}>
+            <div style={{marginBottom: "50px"}}>
                 {this.state.pathway ?
                     <div>
                         <div style={style.headerSpace} className="greenToBlue"/>
                         <div style={style.pathwayHeader}>
-                            { pathway.name }
+                            {pathway.name}
                         </div>
                         <div style={style.contentContainer}>
                             <div className="scrollBarAndContactUs">
                                 <PathwayStepList
                                     className="stepScrollerContainer"
                                     steps={pathway.steps}
-                                    pathwayId={pathway._id} />
+                                    pathwayId={pathway._id}/>
                                 <Paper className="questionsContactUs">
                                     <img
                                         src="/icons/VoiceBubble.png"
-                                        style={{height:"50px", width:"50px", position:"absolute"}}
+                                        style={{height: "50px", width: "50px", position: "absolute"}}
                                     />
-                                    <span style={{fontSize:"20px", marginLeft:"75px"}}>
+                                    <span style={{fontSize: "20px", marginLeft: "75px"}}>
                                         Questions?
                                     </span><br/>
-                                    <p  className="clickable blueText"
-                                        style={{margin:"10px 0px 0px 75px"}}
-                                        onClick={() => this.goTo('/contactUs')}>
+                                    <p className="clickable blueText"
+                                       style={{margin: "10px 0px 0px 75px"}}
+                                       onClick={() => this.goTo('/contactUs')}>
                                         Contact Us
                                     </p>
                                 </Paper>
                             </div>
-                            { content }
+                            {content}
                             <Paper style={style.overviewAndCommentBox}>
                                 <Paper style={{width: "100%"}}>
                                     <ul className="horizCenteredList darkPurpleText smallText2">
@@ -223,7 +228,24 @@ class PathwayContent extends Component {
                                         </li>
                                     </ul>
                                 </Paper>
-                                Here
+                                <div style={{textAlign: "center"}}>
+                                    <Tabs
+                                        style={style.tabs}
+                                        inkBarStyle={{background: '#B869FF'}}
+                                        tabItemContainerStyle={{width: '40%'}}
+                                        className="myPathwaysTabs"
+                                    >
+                                        <Tab label="Overview" style={style.tab}>
+                                            <p className="smallText2 center">{pathway.overview}</p>
+                                        </Tab>
+                                        <Tab label="Exercise Files" style={style.tab}>
+                                            <h1 className="center smallText2">No exercise files yet.</h1>
+                                        </Tab>
+                                        <Tab label="Comments" style={style.tab}>
+                                            <h1 className="center smallText2">No comments yet.</h1>
+                                        </Tab>
+                                    </Tabs>
+                                </div>
                             </Paper>
                         </div>
                     </div>
