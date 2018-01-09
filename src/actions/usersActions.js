a8ab74051647fe5fdf57284487fd220d78bf2dc7
Stephen Dorn <steved11@sbcglobal.net>
2018-01-09 11:47:16 -0600

    fixed login rerouting

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -48,9 +48,7 @@ export function login(user) {
       .then(function(response) {
         dispatch({type:"LOGIN", payload: response.data});
         dispatch({type: "CLOSE_NOTIFICATION"});
-          // TEMPORARY, SWITCH BACK TO PUSHING THEM TO LANDING PAGE
-          browserHistory.push('/onboarding');
-        // browserHistory.push('/discover');
+        browserHistory.push('/discover');
 
         axios.post("/api/userSession", {userId: response.data._id})
             .then(function(response) {
