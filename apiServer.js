66880cb5e722204f4ba59a3c1ded9e2069231311
Stephen Dorn <steved11@sbcglobal.net>
2018-01-18 22:11:18 -0800

    changed to https from http

--- apiServer.js
+++ apiServer.js
@@ -335,10 +335,10 @@ app.post('/sendVerificationEmail', function (req, res) {
              '<div style="font-size:15px;text-align:center;font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;">'
             +   '<a href="https://www.moonshotlearning.org/"><img style="height:100px;margin-bottom:20px"src="https://image.ibb.co/ndbrrm/Official_Logo_Blue.png"/></a><br/>'
             +   '<div style="text-align:justify;width:80%;margin-left:10%;">'
-            +       '<span style="margin-bottom:20px;display:inline-block;">Thank you for joining Moonshot! To get going on your pathways and learning new skills, please <a href="http://www.moonshotlearning.org/verifyEmail?' + user.emailVerificationToken + '">verify your account</a>. Once you verify your account, you can start building your profile. We hope you have a blast!</span><br/>'
+            +       '<span style="margin-bottom:20px;display:inline-block;">Thank you for joining Moonshot! To get going on your pathways and learning new skills, please <a href="https://www.moonshotlearning.org/verifyEmail?' + user.emailVerificationToken + '">verify your account</a>. Once you verify your account, you can start building your profile. We hope you have a blast!</span><br/>'
             +       '<span style="display:inline-block;">If you have any questions or concerns or if you just want to talk about the weather, please feel free to email us at <a href="mailto:Support@MoonshotLearning.com">Support@MoonshotLearning.com</a>.</span><br/>'
             +   '</div>'
-            +   '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border:2px solid #00d2ff;color:#00d2ff;padding:10px 5px 0px;text-decoration:none;margin:20px;" href="http://www.moonshotlearning.org/verifyEmail?'
+            +   '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border:2px solid #00d2ff;color:#00d2ff;padding:10px 5px 0px;text-decoration:none;margin:20px;" href="https://www.moonshotlearning.org/verifyEmail?'
             +   user.emailVerificationToken
             +   '">VERIFY ACCOUNT</a>'
             +   '<div style="text-align:justify;width:80%;margin-left:10%;">'
@@ -516,7 +516,7 @@ app.post('/forgotPassword', function (req, res) {
 
                 foundUser.password = undefined;
                 let content = 'Click this link to change your password: '
-                    + "<a href='http://www.moonshotlearning.org/changePassword?"
+                    + "<a href='https://www.moonshotlearning.org/changePassword?"
                     + newPasswordToken
                     + "'>Click me</a>";
                 sendEmail(recipient, subject, content, function (success, msg) {
