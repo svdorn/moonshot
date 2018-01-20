2ae1562eab09bfafe4d36d073712ecb56fa79ef6
Stephen Dorn <steved11@sbcglobal.net>
2018-01-20 14:46:20 -0600

    remove logger

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 
@@ -12,7 +12,7 @@ import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 
 // STEP 1 create the store
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk);
 // WE WILL PASS INITIAL STATE FROM SERVER STORE
 const initialState = window.INITIAL_STATE;
 const store = createStore(reducers, initialState, middleware);
