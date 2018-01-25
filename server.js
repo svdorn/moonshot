b8e4f11f6fb8f821170fb4401a635021dacc770c
Stephen Dorn <steved11@sbcglobal.net>
2018-01-24 21:44:57 -0600

    redirect

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('*', function(req, res){
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
+app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+});
+
 app.listen(8081, function() {
   console.log("LISTENING ON PORT 8081");
 })
