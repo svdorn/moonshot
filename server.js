08fdbdf7142288c10af5bef1603f8de45edc33a7
Stephen Dorn <steved11@sbcglobal.net>
2018-01-24 21:55:46 -0600

    trying

--- server.js
+++ server.js
@@ -7,16 +7,8 @@ var path = require('path');
 //middleware to define folder for static files
 app.use(express.static('public'))
 
-app.use(function(req, res, next) {
-    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-    }
-    else
-        next();
-});
-
 app.get('*', function(req, res){
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    res.redirect('https://www.moonshotlearning.org' + req.url);
 })
 
 app.listen(8081, function() {
