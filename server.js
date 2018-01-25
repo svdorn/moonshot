ffa39f71fec57b9ab1ff35d2c8ac7fce509d49e7
Stephen Dorn <steved11@sbcglobal.net>
2018-01-24 21:53:13 -0600

    tryin new things

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var path = require('path');
 //middleware to define folder for static files
 app.use(express.static('public'))
 
-app.get('*', function(req, res){
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
-
 app.use(function(req, res, next) {
     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
         res.redirect('https://' + req.get('Host') + req.url);
@@ -18,6 +14,10 @@ app.use(function(req, res, next) {
     else
         next();
 });
+
+app.get('*', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 app.listen(8081, function() {
   console.log("LISTENING ON PORT 8081");
