35fb7e1155144f737af62b68afd4cf34a5d43116
Austin Meyer <frizzkitten@gmail.com>
2018-01-29 19:11:32 -0600

    more cleaning

--- src/reducers/usersReducers.js
+++ src/reducers/usersReducers.js
@@ -39,10 +39,14 @@ export function usersReducers(state = initialState, action) {
             break;
         case "NOTIFICATION":
         case "LOGIN_REJECTED":
+        case "VERIFY_EMAIL_REJECTED":
+        case "UPDATE_USER_REJECTED":
+        case "CHANGE_PASSWORD":
+        case "CHANGE_PASSWORD_REJECTED":
             return {...state, notification: action.notification};
             break;
         case "SIGNOUT":
-            return {...state, currentUser:undefined};
+            return {...state, currentUser: undefined};
             break;
         case "FORGOT_PASSWORD_REQUESTED":
         case "POST_USER_REQUESTED":
@@ -71,11 +75,6 @@ export function usersReducers(state = initialState, action) {
                 loadingSomething: false
             };
             break;
-        case "VERIFY_EMAIL_REJECTED":
-            return {
-                ...state,
-                notification: action.notification
-            }
         case "RESET_BUTTON":
             return {
                 ...state,
@@ -85,44 +84,15 @@ export function usersReducers(state = initialState, action) {
             };
             break;
         case "DELETE_USER":
-            // Create a copy of the current array of users
-            const currentUserToDelete = [...state.users];
-            // Determine at which index in users array is
-            // the user to be deleted
-            const indexToDelete = currentUserToDelete.findIndex(
-                function (user) {
-                    return user._id == action.payload;
-                }
-            )
-            // use slice to remove the user at the specified index
-            return {
-                users: [...currentUserToDelete.slice(0, indexToDelete),
-                    ...currentUserToDelete.slice(indexToDelete + 1)]
-            }
+            // TODO
             break;
-
         case "UPDATE_USER":
-            // Create a copy of the current array of users
             return {
                 ...state, currentUser: action.payload, notification: action.notification
             };
             break;
-        case "UPDATE_USER_REJECTED":
-            return {
-                ...state, notification: action.notification
-            };
-            break;
-        case "CHANGE_PASSWORD":
-            return {
-                ...state, notification: action.notification
-            };
-            break;
-        case "CHANGE_PASSWORD_REJECTED":
-            return {
-                ...state, notification: action.notification
-            };
-            break;
         case "FOR_BUSINESS_REQUESTED":
+        case "CONTACT_US_REQUESTED":
             return {
                 ...state, loadingSomething: true
             };
@@ -134,12 +104,10 @@ export function usersReducers(state = initialState, action) {
             }
             return newState;
             break;
-        case "CONTACT_US_REQUESTED":
-            return {
-                ...state, loadingSomething: true
-            };
-            break;
         case "CONTACT_US":
+        case "FORGOT_PASSWORD":
+        case "FORGOT_PASSWORD_REJECTED":
+        case "FORM_ERROR":
             return {
                 ...state, notification: action.notification, loadingSomething: false
             };
@@ -152,21 +120,6 @@ export function usersReducers(state = initialState, action) {
         case "REGISTER_FOR_PATHWAY_REQUESTED":
             return {
                 ...state, loadingEmail: true
-            };
-            break;
-        case "FORGOT_PASSWORD":
-            return {
-                ...state, notification: action.notification, loadingSomething: false
-            };
-            break;
-        case "FORGOT_PASSWORD_REJECTED":
-            return {
-                ...state, notification: action.notification, loadingSomething: false
-            };
-            break;
-        case "FORM_ERROR":
-            return {
-                ...state, notification: action.notification, loadingSomething: false
             };
             break;
         case "UPDATE_CURRENT_SUBSTEP":
