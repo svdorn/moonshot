9de0f689dabbc35d72122300f00e4dbbc2095345
Stephen Dorn <steved11@sbcglobal.net>
2018-01-30 16:55:55 -0600

    are you an employer on home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -403,6 +403,7 @@ class Home extends Component {
                                 Start Profile
                             </div>
                         </button>
+                        <div className="clickable blueText" style={{marginTop: '10px'}} onClick={() => this.goTo('/forBusiness')}><i>Are you an employer?</i></div>
                     </div>
                 </div>
             </div>
