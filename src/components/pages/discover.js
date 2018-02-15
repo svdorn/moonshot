384eb5ea89b4dc2ee55a4a34cf39e804f8ce2a32
Stephen Dorn <steved11@sbcglobal.net>
2018-02-15 16:29:28 -0600

    can only see pathways through link

--- src/components/pages/discover.js
+++ src/components/pages/discover.js
@@ -199,24 +199,25 @@ class Discover extends Component {
             const deadline = new Date(pathway.deadline);
             const formattedDeadline = deadline.getMonth() + "/" + deadline.getDate() + "/" + deadline.getYear();
             if (!pathway.comingSoon) {
-                return (
-                    <li className="pathwayPreviewLi explorePathwayPreview"
-                        key={key}
-                        onClick={() => self.goTo('/pathway?' + pathway.url)}
-                    >
-                        <PathwayPreview
-                            name={pathway.name}
-                            image={pathway.previewImage}
-                            logo = {pathway.sponsor.logo}
-                            sponsorName = {pathway.sponsor.name}
-                            completionTime={pathway.estimatedCompletionTime}
-                            deadline={formattedDeadline}
-                            price={pathway.price}
-                            _id={pathway._id}
-                            comingSoon = {pathway.comingSoon}
-                        />
-                    </li>
-                );
+                return;
+                // return (
+                //     <li className="pathwayPreviewLi explorePathwayPreview"
+                //         key={key}
+                //         onClick={() => self.goTo('/pathway?' + pathway.url)}
+                //     >
+                //         <PathwayPreview
+                //             name={pathway.name}
+                //             image={pathway.previewImage}
+                //             logo = {pathway.sponsor.logo}
+                //             sponsorName = {pathway.sponsor.name}
+                //             completionTime={pathway.estimatedCompletionTime}
+                //             deadline={formattedDeadline}
+                //             price={pathway.price}
+                //             _id={pathway._id}
+                //             comingSoon = {pathway.comingSoon}
+                //         />
+                //     </li>
+                // );
             } else if (pathway.comingSoon) {
                 return (
                     <li className="pathwayPreviewLi explorePathwayPreview"
