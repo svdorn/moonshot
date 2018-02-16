1d2d0a5161e836d10336ca0170e123a7129e670d
Austin Meyer <frizzkitten@gmail.com>
2018-02-16 14:32:44 -0600

    circular progress for quizzes

--- src/components/childComponents/pathwayContent/pathwayContentQuiz.js
+++ src/components/childComponents/pathwayContent/pathwayContentQuiz.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Paper} from 'material-ui';
+import {Paper, CircularProgress} from 'material-ui';
 import {connect} from 'react-redux';
 import axios from 'axios';
 import PathwayContentSliderQuestion from './pathwayContentSliderQuestion';
@@ -31,7 +31,7 @@ class PathwayContentQuiz extends Component {
     }
 
     componentDidUpdate() {
-            // load content if it isn't already loaded
+        // load content if it isn't already loaded
         if (this.props.step !== this.state.currStep) {
             const id = this.props.step.contentID;
 
@@ -49,54 +49,52 @@ class PathwayContentQuiz extends Component {
 
     render() {
         const quiz = this.state.quiz;
+        let questionJsx = "Error getting question";
         // if the quiz hasn't yet been received from back end, don't show anything
         if (!quiz || !quiz.questionType) {
-            return null;
-        }
-
-        const sliderOptions = quiz.sliderOptions ? quiz.sliderOptions : {};
-        const questionType = quiz.questionType;
-        let questionJsx = null;
-        switch (questionType) {
-            case "slider":
-                questionJsx =
-                    <PathwayContentSliderQuestion
-                        question={quiz.question}
-                        minValue={quiz.sliderOptions.minValue}
-                        maxValue={quiz.sliderOptions.maxValue}
-                        initialValue={quiz.sliderOptions.initialValue}
-                        step={quiz.sliderOptions.step}
-                        quizId={quiz._id}
-                    />
-                break;
-            case "twoOptions":
-                questionJsx =
-                    <PathwayContentTwoOptionsQuestion
-                        question={quiz.question}
-                        choices={quiz.twoOptionsChoices}
-                        quizId={quiz._id}
-                    />
-                break;
-            case "multipleChoice":
-                questionJsx =
-                    <PathwayContentMultipleChoiceQuestion
-                        question={quiz.question}
-                        answers={quiz.multipleChoiceAnswers}
-                        allowCustomAnswer={quiz.allowCustomAnswer}
-                        quizId={quiz._id}
-                    />
-                break;
-            default:
-                return null;
+            questionJsx = <CircularProgress style={{marginTop:"40px"}}/>;
+        } else {
+            const sliderOptions = quiz.sliderOptions ? quiz.sliderOptions : {};
+            const questionType = quiz.questionType;
+            switch (questionType) {
+                case "slider":
+                    questionJsx =
+                        <PathwayContentSliderQuestion
+                            question={quiz.question}
+                            minValue={quiz.sliderOptions.minValue}
+                            maxValue={quiz.sliderOptions.maxValue}
+                            initialValue={quiz.sliderOptions.initialValue}
+                            step={quiz.sliderOptions.step}
+                            quizId={quiz._id}
+                        />
+                    break;
+                case "twoOptions":
+                    questionJsx =
+                        <PathwayContentTwoOptionsQuestion
+                            question={quiz.question}
+                            choices={quiz.twoOptionsChoices}
+                            quizId={quiz._id}
+                        />
+                    break;
+                case "multipleChoice":
+                    questionJsx =
+                        <PathwayContentMultipleChoiceQuestion
+                            question={quiz.question}
+                            answers={quiz.multipleChoiceAnswers}
+                            allowCustomAnswer={quiz.allowCustomAnswer}
+                            quizId={quiz._id}
+                        />
+                    break;
+                default:
+                    break;
+            }
         }
 
         return (
             <div className={this.props.className} style={{...this.props.style}}>
-                {this.state.quiz !== undefined ?
-                    <div className="center" style={{margin: "10px 0"}}>
-                        { questionJsx }
-                    </div>
-                    : null}
+                <div className="center" style={{margin: "10px 0"}}>
+                    { questionJsx }
+                </div>
             </div>
         );
     }
