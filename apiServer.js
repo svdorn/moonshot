a26ed11835aba33cf3c4439b2aeecb4616f02d21
Austin Meyer <frizzkitten@gmail.com>
2018-02-19 17:48:03 -0600

    sends verification email with opt out info

--- apiServer.js
+++ apiServer.js
@@ -600,6 +600,7 @@ app.post('/user/changePasswordForgot', function (req, res) {
     });
 });
 
+
 // SEND EMAIL
 app.post('/sendVerificationEmail', function (req, res) {
     let email = sanitize(req.body.email);
@@ -610,17 +611,21 @@ app.post('/sendVerificationEmail', function (req, res) {
         let subject = 'Verify email';
         let content =
             '<div style="font-size:15px;text-align:center;font-family: Arial, sans-serif;color:#686868">'
-            + '<a href="https://www.moonshotlearning.org/" style="color:#00c3ff"><img style="height:100px;margin-bottom:20px"src="https://image.ibb.co/ndbrrm/Official_Logo_Blue.png"/></a><br/>'
-            + '<div style="text-align:justify;width:80%;margin-left:10%;">'
-            + '<span style="margin-bottom:20px;display:inline-block;">Thank you for joining Moonshot! To get going on your pathways and learning new skills, please <a href="https://www.moonshotlearning.org/verifyEmail?' + user.emailVerificationToken + '">verify your account</a>. Once you verify your account, you can start building your profile. We hope you have a blast!</span><br/>'
-            + '<span style="display:inline-block;">If you have any questions or concerns or if you just want to talk about the weather, please feel free to email us at <a href="mailto:Support@MoonshotLearning.org">Support@MoonshotLearning.com</a>.</span><br/>'
-            + '</div>'
-            + '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border:2px solid #00d2ff;color:#00d2ff;padding:10px 5px 0px;text-decoration:none;margin:20px;" href="https://www.moonshotlearning.org/verifyEmail?'
-            + user.emailVerificationToken
-            + '">VERIFY ACCOUNT</a>'
-            + '<div style="text-align:left;width:80%;margin-left:10%;">'
-            + '<span style="margin-bottom:20px;display:inline-block;">On behalf of the Moonshot Team, we welcome you to our family and look forward to helping you pave your future and shoot for the stars.</span><br/>'
-            + '</div>'
+                + '<a href="https://www.moonshotlearning.org/" style="color:#00c3ff"><img style="height:100px;margin-bottom:20px"src="https://image.ibb.co/iAchLn/Official_Logo_Blue.png"/></a><br/>'
+                    + '<div style="text-align:justify;width:80%;margin-left:10%;">'
+                    + '<span style="margin-bottom:20px;display:inline-block;">Thank you for joining Moonshot! To get going on your pathways, learning new skills, and building your profile for employers, please <a href="https://www.moonshotlearning.org/verifyEmail?' + user.emailVerificationToken + '">verify your account</a>. Once you verify your account, you can start building your profile. We hope you have a blast!</span><br/>'
+                    + '<span style="display:inline-block;">If you have any questions or concerns or if you just want to talk about the weather, please feel free to email us at <a href="mailto:Support@MoonshotLearning.org">Support@MoonshotLearning.com</a>.</span><br/>'
+                    + '</div>'
+                + '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border:2px solid #00d2ff;color:#00d2ff;padding:10px 5px 0px;text-decoration:none;margin:20px;" href="https://www.moonshotlearning.org/verifyEmail?'
+                + user.emailVerificationToken
+                + '">VERIFY ACCOUNT</a>'
+                + '<div style="text-align:left;width:80%;margin-left:10%;">'
+                    + '<span style="margin-bottom:20px;display:inline-block;">On behalf of the Moonshot Team, we welcome you to our family and look forward to helping you pave your future and shoot for the stars.</span><br/>'
+                    + '<div style="font-size:10px; text-align:center; color:#C8C8C8; margin-bottom:30px;">'
+                    + '<i>Moonshot Learning, Inc.<br/><a href="" style="text-decoration:none;color:#C8C8C8;">1261 Meadow Sweet Dr<br/>Madison, WI 53719</a>.<br/>'
+                    + '<a style="color:#C8C8C8;" href="https://www.moonshotlearning.org/unsubscribe?' + user.email + '">Opt-out of future messages.</a></i>'
+                    + '</div>'
+                + '</div>'
             + '</div>';
 
         sendEmail(recipient, subject, content, function (success, msg) {
