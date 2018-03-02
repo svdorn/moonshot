43329249b26863693a524ae27dd8cea7f2e92677
Austin Meyer <frizzkitten@gmail.com>
2018-03-01 19:48:52 -0600

    updated terms of use

--- src/components/pages/termsOfUse.js
+++ src/components/pages/termsOfUse.js
@@ -8,69 +8,61 @@ class TermsOfUse extends Component {
             <div>
             <div className="purpleToRedLightGradient termsPage">
 
-<br/><b>            Moonshot’s Terms of Use ("Terms")
-</b>
-<br/>            Last updated: 3/1/2018
+<br/>            Moonshot’s Terms of Use ("Terms")
 
-<br/>            Please read these Terms of Use ("Terms", "Terms of Use") carefully before using moonshotlearning.org (“website”, “site”) operated by Moonshot Learning, Inc. (“Moonshot”, "us", "we", or "our").
+<br/>             Last updated: 3/1/2018
 
-<br/>            Your access to and use of the Site is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Site.
+<br/>             Please read these Terms of Use ("Terms", "Terms of Use") carefully before using moonshotlearning.org (“website”, “site”) operated by Moonshot Learning, Inc. (“Moonshot”, "us", "we", or "our").
 
-<br/>            By accessing or using the Site you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Site.
+<br/>             Your access to and use of the Site is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Site.
 
-<br/><b>            Termination
-</b>
-<br/>            We may terminate your access to the Site, without cause or notice, which may result in the forfeiture and destruction of all information associated with your account. All provisions of this Agreement that, by their nature, should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.
+<br/>             By accessing or using the Site you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Site.
 
-<br/><b>            Content
-</b>
-<br/>            Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the use of that content and its accessibility through our site. You can delete all of your data on Moonshot by emailing support@moonshotlearning.org.
+<br/>             Age Requirement
 
-<br/><b>            Links To Other Websites
-</b>
-<br/>            Our Service may contain links to third-party web sites or services that are not owned or controlled by Moonshot. Moonshot has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party websites or services. You further acknowledge and agree that Moonshot shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such websites or services.
+<br/>             You must be at least 13 years old to create an account with Moonshot.
 
-<br/><b>            Age Requirement
-</b>
-<br/>            You must be at least 13 years old to create an account with Moonshot.
+<br/>             Content
 
-<br/><b>            Intellectual Property
-</b>
-<br/>            The Site and its original content, features, and functionality are owned by Moonshot and are protected by international copyright, trademark, patent, trade secret and other intellectual property or proprietary rights laws.
+<br/>             Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the use of that content and its accessibility through our site. You can delete all of your data on Moonshot by emailing support@moonshotlearning.org.
 
-<br/><b>            User Limitation
-</b>
-<br/>            By accepting these terms, you agree not to do anything unlawful or violate any of these terms.
+<br/>             Links To Other Websites
 
-<br/><b>            Warranty Disclaimer
-</b>
-<br/>            Moonshot does not guarantee that the information provided on our website is accurate, complete or suitable for any purpose. Moonshot cannot be held responsible for any errors in the content engaged through moonshotlearning.org.
+<br/>             Our Service may contain links to third-party web sites or services that are not owned or controlled by Moonshot. Moonshot has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party websites or services. You further acknowledge and agree that Moonshot shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such websites or services.
 
-<br/><b>            Incentive Disclaimer
-</b>
-<br/>            Moonshot’s $100 if you interview user incentive applies only when users complete a Moonshot pathway. The candidate must be interviewed directly by one of Moonshot’s hiring partners for the $100 to apply. Any and all engagements by the candidate to initiate the interview outside of Moonshot nullifies the $100 if you interview incentive. Each user can receive the $100 incentive once.
+<br/>             Termination
 
-<br/><b>            Results Disclaimer
-</b>
-<br/>            Moonshot does not guarantee that users will get hired by Moonshot’s hiring partners.
+<br/>             We may terminate your access to the Site, without cause or notice, which may result in the forfeiture and destruction of all information associated with your account. All provisions of this Agreement that, by their nature, should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.
 
-<br/><b>            Changes
-</b>
-<br/>            We reserve the right, at our sole discretion, to modify or replace these Terms at any time. Moonshot will notify you when and if Terms have been changed.
+<br/>             Intellectual Property
 
-<br/><b>            Governing Law
-</b>
-<br/>            These terms and conditions are governed by the laws of the United States of America and the laws of the State of Wisconsin.
+<br/>             The Site and its original content, features, and functionality are owned by Moonshot and are protected by international copyright, trademark, patent, trade secret and other intellectual property or proprietary rights laws.
 
-<br/><b>            Privacy Policy
-</b>
-<br/>            We collect your name, phone number, email, other personally identifiable information and data collected through commission of you completing Moonshot pathways.
+<br/>             User Limitation
 
-<br/>            We collect this information through your submission on our site and by completing pathways, engaging with other content and sources through our site. By accepting this agreement, you allow us to collect your data as you engage with content on and linked to our site.
+<br/>             By accepting these terms, you agree not to do anything unlawful or violate any of these terms.
 
-<br/>            All of this data will only be used, in our sole discretion, to advance the chances of you and other users on the site to receive employment opportunities with our existing and potential hiring partners.
+<br/>             Warranty Disclaimer
 
-<br/>            Data is protected by being stored on our servers and secure databases. You can control your data in profile settings and request for all of your data on Moonshot to be deleted by contacting support@moonshotlearning.org.
+<br/>             Moonshot does not guarantee that the information provided on our website is accurate, complete or suitable for any purpose. Moonshot cannot be held responsible for any errors in the content engaged through moonshotlearning.org.
+
+<br/>             Incentive Disclaimer
+
+<br/>             Moonshot’s $100 if you interview user incentive applies only when users complete a Moonshot pathway. $100 if you interview incentives only apply to a select set of pathways. To receive the incentive, the candidate must be interviewed directly by one of Moonshot’s hiring partners for the $100 to apply. Any and all engagements by the candidate to initiate the interview outside of Moonshot nullifies the $100 if you interview incentive. Each user can receive the $100 incentive once.
+
+<br/>             Results Disclaimer
+
+<br/>             Moonshot does not guarantee that users will get hired by Moonshot’s hiring partners.
+
+<br/>             Changes
+
+<br/>             We reserve the right, at our sole discretion, to modify or replace these Terms at any time. Moonshot will notify you when and if Terms have been changed.
+
+<br/>             Governing Law
+
+<br/>             These terms and conditions are governed by the laws of the United States of America and the laws of the State of Wisconsin.
+
+
 
 
 
