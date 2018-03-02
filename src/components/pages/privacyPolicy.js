90f9976a6c5c530eaaa0cccb0e0f09214b019ad8
Stephen Dorn <steved11@sbcglobal.net>
2018-03-02 14:07:20 -0600

    dialog for terms of use and privacy policy

--- src/components/pages/privacyPolicy.js
+++ src/components/pages/privacyPolicy.js
@@ -1,72 +1,101 @@
 "use strict"
-import React, { Component } from 'react';
-import { Paper } from 'material-ui';
+import React, {Component} from 'react';
+import {Paper} from 'material-ui';
 
 class PrivacyPolicy extends Component {
-    render () {
+    render() {
         return (
             <div>
-            <div className="purpleToRedLightGradient termsPage">
+                <b className="font20px font18pxUnder700">Moonshot’s Privacy Policy</b>
 
-                <br/><b>Moonshot’s Privacy Policy</b>
+                <br/><br/>Last updated: 3/2/2018
 
-<br/>Last updated: 3/2/2018
+                <br/><br/>Moonshot Learning, Inc. ("us", "we", or "our") operates moonshotlearning.org ("website",
+                "site"). This Privacy Policy informs you of our policies regarding the collection, use and disclosure of
+                Personal Information we receive from users of the site.
 
-<br/>Moonshot Learning, Inc. ("us", "we", or "our") operates moonshotlearning.org ("website", "site"). This Privacy Policy informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from users of the site.
+                <br/><br/>By using this site, you agree to the collection and use of information in accordance with this
+                policy.
 
-<br/>By using this site, you agree to the collection and use of information in accordance with this policy.
+                <br/><br/><b>Information Collection and Use</b>
 
-                <br/><b>Information Collection and Use</b>
+                <br/><br/>While using our site, we may ask you to provide us with certain personally identifiable
+                information that can be used to contact or identify you. Personally identifiable information may
+                include, but is not limited to your name, email address and phone number.
 
-<br/>While using our site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to your name, email address and phone number.
+                <br/><br/><b>Log Data</b>
 
-                <br/><b>Log Data</b>
+                <br/><br/>Like many site operators, we collect information that your browser sends whenever you visit
+                our site (“Log Data”). This Log Data may include information such as your computer’s Internet Protocol
+                (“IP”) address, browser type, browser version, the pages of our site that you visit, the time and date
+                you visit, the time spent on those pages and other statistics.
 
-<br/>Like many site operators, we collect information that your browser sends whenever you visit our site (“Log Data”). This Log Data may include information such as your computer’s Internet Protocol (“IP”) address, browser type, browser version, the pages of our site that you visit, the time and date you visit, the time spent on those pages and other statistics.
+                <br/><br/><b>Communications</b>
 
-                <br/><b>Communications</b>
+                <br/><br/>We may use your Personal Information to contact you with newsletters, marketing or promotional
+                materials and other information.
 
-<br/>We may use your Personal Information to contact you with newsletters, marketing or promotional materials and other information.
+                <br/><br/><b>Cookies</b>
 
-                <br/><b>Cookies</b>
+                <br/><br/>Cookies are files with small amount of data, which may include an anonymous unique identifier.
+                Cookies are sent to your browser from a web site and stored on your computer{"'"}s hard drive.
 
-<br/>Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer{"'"}s hard drive.
+                <br/><br/>Like many sites, we use "cookies" to collect information. You can instruct your browser to
+                refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies,
+                you may not be able to use some portions of our Site.
 
-<br/>Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Site.
+                <br/><br/><b>Security</b>
 
-                <br/><b>Security</b>
+                <br/><br/>The security of your Personal Information is important to us. We are responsible for the
+                accuracy and security of your personally identifiable data. We use commercially acceptable means to
+                protect your Personal Information to ensure unauthorized parties cannot access your data.
 
-<br/>The security of your Personal Information is important to us. We are responsible for the accuracy and security of your personally identifiable data. We use commercially acceptable means to protect your Personal Information to ensure unauthorized parties cannot access your data.
+                <br/><br/><b>Changes To This Privacy Policy</b>
 
-                <br/><b>Changes To This Privacy Policy</b>
+                <br/><br/>This Privacy Policy will remain in effect except with respect to any changes in its provisions
+                in the future, which will be in effect immediately after being posted on this page.
 
-<br/>This Privacy Policy will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.
+                <br/><br/>We reserve the right to update or change our Privacy Policy at any time and you should check
+                this Privacy Policy periodically. Your continued use of our Site after we post any modifications to the
+                Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to
+                abide and be bound by the modified Privacy Policy.
 
-<br/>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of our Site after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.
+                <br/><br/>If we make any material changes to this Privacy Policy, we will notify you either through the
+                email address you have provided us, or by placing a prominent notice on our website.
 
-<br/>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.
+                <br/><br/><b>Links</b>
 
-                <br/><b>Links</b>
-                <br/>This website contains links to other sites. Please be aware that we are not responsible for the content or privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of any other site that collects personally identifiable information.
+                <br/><br/>This website contains links to other sites. Please be aware that we are not responsible for
+                the content or privacy practices of such other sites. We encourage our users to be aware when they leave
+                our site and to read the privacy statements of any other site that collects personally identifiable
+                information.
 
-                <br/><b>Data Collection and Use</b>
+                <br/><br/><b>Data Collection and Use</b>
 
-<br/>We collect your name, phone number, email, other personally identifiable information and data collected through commission of you completing Moonshot pathways. Data is protected by being stored on our servers and secure databases.
+                <br/><br/>We collect your name, phone number, email, other personally identifiable information and data
+                collected through commission of you completing Moonshot pathways. Data is protected by being stored on
+                secure servers and databases.
 
-<br/>We collect this information through your submission on our site and by completing pathways, engaging with other content and sources through our site. By accepting this agreement, you allow us to collect your data as you engage with content on and linked to our site.
+                <br/><br/>We collect this information through your submission on our site and by completing pathways,
+                engaging with other content and sources through our site. By accepting this agreement, you allow us to
+                collect your data as you engage with content on and linked to our site.
 
-<br/>All of this data will only be used, in our sole discretion, to advance the chances of you and other users on the site to receive employment opportunities with our existing and potential hiring partners. The purpose of the data is employment so you allow us to share your data with various employers, organizations and other stakeholders to technology and the workforce.
+                <br/><br/>All of this data will only be used, in our sole discretion, to advance the chances of you and
+                other users on the site to receive employment opportunities with our existing and potential hiring
+                partners. The purpose of the data is employment so you allow us to share your data with various
+                employers, organizations and other stakeholders to technology and the workforce.
 
-                <br/><b>Control Over Your Data</b>
+                <br/><br/><b>Control Over Your Data</b>
 
-                <br/>You can control your basic data in profile settings, exercise choice over what data we collect, contact us to see and contest all of the personally identifiable data that Moonshot has collected from you and request for all of your data on Moonshot to be deleted, at any time, by contacting support@moonshotlearning.org.
+                <br/><br/>You can control your basic data in profile settings, exercise choice over what data we
+                collect, contact us to see and contest all of the personally identifiable data that Moonshot has
+                collected from you and request for all of your data on Moonshot to be deleted, at any time, by
+                contacting support@moonshotlearning.org.
 
-<br/><b>Contact Us</b>
+                <br/><br/><b>Contact Us</b>
 
-<br/>If you have any questions about this Privacy Policy, please contact us at support@moonshotlearning.org.
-
-
-            </div>
+                <br/><br/>If you have any questions about this Privacy Policy, please contact us at
+                support@moonshotlearning.org.
             </div>
         );
     }
--- src/components/pages/termsOfUse.js
+++ src/components/pages/termsOfUse.js
@@ -1,72 +1,92 @@
 "use strict"
-import React, { Component } from 'react';
-import { Paper } from 'material-ui';
+import React, {Component} from 'react';
+import {Paper} from 'material-ui';
 
 class TermsOfUse extends Component {
-    render () {
+    render() {
         return (
             <div>
-            <div className="purpleToRedLightGradient termsPage">
 
-                <br/>            <b>Moonshot’s Terms of Use ("Terms")</b>
+                <b className="font20px font18pxUnder700px">Moonshot’s Terms of Use ("Terms")</b>
 
-<br/>             Last updated: 3/2/2018
+                <br/><br/> Last updated: 3/2/2018
 
-<br/>             Please read these Terms of Use ("Terms", "Terms of Use") carefully before using moonshotlearning.org (“website”, “site”) operated by Moonshot Learning, Inc. (“Moonshot”, "us", "we", or "our").
+                <br/><br/> Please read these Terms of Use ("Terms", "Terms of Use") carefully before using
+                moonshotlearning.org (“website”, “site”) operated by Moonshot Learning, Inc. (“Moonshot”, "us", "we", or
+                "our").
 
-<br/>             Your access to and use of the Site is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Site.
+                <br/><br/> Your access to and use of the Site is conditioned on your acceptance of and compliance with
+                these Terms. These Terms apply to all visitors, users and others who access or use the Site.
 
-<br/>             By accessing or using the Site you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Site.
+                <br/><br/> By accessing or using the Site you agree to be bound by these Terms. If you disagree with any
+                part of the terms then you may not access the Site.
 
-                <br/>             <b>Age Requirement</b>
+                <br/><br/> <b>Age Requirement</b>
 
-<br/>             You must be at least 13 years old to create an account with Moonshot.
+                <br/><br/> You must be at least 13 years old to create an account with Moonshot.
 
-                <br/>             <b>Content</b>
+                <br/><br/> <b>Content</b>
 
-<br/>             Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the use of that content and its accessibility through our site. You can delete all of your data on Moonshot by emailing support@moonshotlearning.org.
+                <br/><br/> Our Service allows you to post, link, store, share and otherwise make available certain
+                information, text, graphics, videos, or other material ("Content"). You are responsible for the use of
+                that content and its accessibility through our site. You can delete all of your data on Moonshot by
+                emailing support@moonshotlearning.org.
 
-                <br/>             <b>Links To Other Websites</b>
+                <br/><br/> <b>Links To Other Websites</b>
 
-<br/>             Our Service may contain links to third-party web sites or services that are not owned or controlled by Moonshot. Moonshot has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party websites or services. You further acknowledge and agree that Moonshot shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such websites or services.
+                <br/><br/> Our Service may contain links to third-party web sites or services that are not owned or
+                controlled by Moonshot. Moonshot has no control over, and assumes no responsibility for, the content,
+                privacy policies, or practices of any third party websites or services. You further acknowledge and
+                agree that Moonshot shall not be responsible or liable, directly or indirectly, for any damage or loss
+                caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or
+                services available on or through any such websites or services.
 
-                <br/>             <b>Termination</b>
+                <br/><br/> <b>Termination</b>
 
-<br/>             We may terminate your access to the Site, without cause or notice, which may result in the forfeiture and destruction of all information associated with your account. All provisions of this Agreement that, by their nature, should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.
+                <br/><br/> We may terminate your access to the Site, without cause or notice, which may result in the
+                forfeiture and destruction of all information associated with your account. All provisions of this
+                Agreement that, by their nature, should survive termination shall survive termination, including,
+                without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.
 
-                <br/>             <b>Intellectual Property</b>
+                <br/><br/> <b>Intellectual Property</b>
 
-<br/>             The Site and its original content, features, and functionality are owned by Moonshot and are protected by international copyright, trademark, patent, trade secret and other intellectual property or proprietary rights laws.
+                <br/><br/> The Site and its original content, features, and functionality are owned by Moonshot and are
+                protected by international copyright, trademark, patent, trade secret and other intellectual property or
+                proprietary rights laws.
 
-                <br/>             <b>User Limitation</b>
+                <br/><br/> <b>User Limitation</b>
 
-<br/>             By accepting these terms, you agree not to do anything unlawful or violate any of these terms.
+                <br/><br/> By accepting these terms, you agree not to do anything unlawful or violate any of these
+                terms.
 
-                <br/>             <b>Warranty Disclaimer</b>
+                <br/><br/> <b>Warranty Disclaimer</b>
 
-<br/>             Moonshot does not guarantee that the information provided on our website is accurate, complete or suitable for any purpose. Moonshot cannot be held responsible for any errors in the content engaged through moonshotlearning.org.
+                <br/><br/> Moonshot does not guarantee that the information provided on our website is accurate,
+                complete or suitable for any purpose. Moonshot cannot be held responsible for any errors in the content
+                engaged through moonshotlearning.org.
 
-                <br/>             <b>Incentive Disclaimer</b>
+                <br/><br/> <b>Incentive Disclaimer</b>
 
-<br/>             Moonshot’s $100 if you interview user incentive applies only when users complete a Moonshot pathway. $100 if you interview incentives only apply to a select set of pathways. To receive the incentive, the candidate must be interviewed directly by one of Moonshot’s hiring partners for the $100 to apply. Any and all engagements by the candidate to initiate the interview outside of Moonshot nullifies the $100 if you interview incentive. Each user can receive the $100 incentive once.
+                <br/><br/> Moonshot’s $100 if you interview user incentive applies only when users complete a Moonshot
+                pathway. $100 if you interview incentives only apply to a select set of pathways. To receive the
+                incentive, the candidate must be interviewed directly by one of Moonshot’s hiring partners for the $100
+                to apply. Any and all engagements by the candidate to initiate the interview outside of Moonshot
+                nullifies the $100 if you interview incentive. Each user can receive the $100 incentive once.
 
-                <br/>             <b>Results Disclaimer</b>
+                <br/><br/> <b>Results Disclaimer</b>
 
-<br/>             Moonshot does not guarantee that users will get hired by Moonshot’s hiring partners.
+                <br/><br/> Moonshot does not guarantee that users will get hired by Moonshot’s hiring partners.
 
-                <br/>             <b>Changes</b>
+                <br/><br/> <b>Changes</b>
 
-<br/>             We reserve the right, at our sole discretion, to modify or replace these Terms at any time. Moonshot will notify you when and if Terms have been changed.
+                <br/><br/> We reserve the right, at our sole discretion, to modify or replace these Terms at any time.
+                Moonshot will notify you when and if Terms have been changed.
 
-                <br/>             <b>Governing Law</b>
+                <br/><br/> <b>Governing Law</b>
 
-<br/>             These terms and conditions are governed by the laws of the United States of America and the laws of the State of Wisconsin.
+                <br/><br/> These terms and conditions are governed by the laws of the United States of America and the
+                laws of the State of Wisconsin.
 
-
-
-
-
-            </div>
             </div>
         );
     }
