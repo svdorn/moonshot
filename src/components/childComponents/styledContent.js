d6bef338f019db1d9835a3ede8e45a4182feb8f4
Austin Meyer <frizzkitten@gmail.com>
2018-03-04 16:18:03 -0600

    const should have been let

--- src/components/childComponents/styledContent.js
+++ src/components/childComponents/styledContent.js
@@ -9,7 +9,7 @@ class StyledContent extends Component {
         let contentHtml = [];
         contentArray.forEach(function(part) {
             // default classNames; if className provided, give the part that className instead
-            const defaultClassNames = "inlineBlock font20px font14pxUnder600 marginSides80px marginSides40pxUnder700 marginSides20pxUnder400";
+            let defaultClassNames = "inlineBlock font20px font14pxUnder600 marginSides80px marginSides40pxUnder700 marginSides20pxUnder400";
             if (part.partType === "code") {
                 defaultClassNames = "inlineBlock font16px font12pxUnder600 marginSides80px marginSides40pxUnder700 marginSides20pxUnder400";
             }
