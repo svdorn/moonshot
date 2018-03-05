1d4375baf5ce8d8ee73aaa65923ac8a034727630
Austin Meyer <frizzkitten@gmail.com>
2018-03-04 22:03:53 -0600

    able to do that in db too

--- models/videos.js
+++ models/videos.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 var videosSchema = mongoose.Schema({
     link: String,
     name: String,
+    start: String,
+    end: String,
+    showAnnotations: Boolean
 });
 
 var Videos = mongoose.model('Videos', videosSchema);
