1de3bb540d14838d5ba47fd8ce030e5fd974508f
Stephen Dorn <steved11@sbcglobal.net>
2018-03-10 12:33:26 -0600

    stepper alignment

--- src/components/childComponents/candidatePreview.js
+++ src/components/childComponents/candidatePreview.js
@@ -1,22 +1,39 @@
 import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
-import {Paper} from 'material-ui';
+import {Paper, Stepper, Step, StepLabel} from 'material-ui';
 
 class PathwayPreview extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            step: 1,
+        }
     }
 
     render() {
         return (
             <div className="candidatePreview">
-                <Paper className="candidatePreviewPaper aboutMeLi font20px font font16pxUnder700 font14pxUnder400" zDepth={2}>
+                <Paper className="candidatePreviewPaper candidatePreviewLi font16px font font14pxUnder700 font12pxUnder400" zDepth={2}>
                     <div className="candidatePreviewLiLeftContainer">{this.props.name}</div>
 
                     <div className="verticalDividerCandidatePreview"/>
 
                     <div className="candidatePreviewLiInfo" style={{display: 'inline-block'}}>
-                        Here
+                        <Stepper activeStep={this.state.step}>
+                            <Step>
+                                <StepLabel>Not Yet Contacted</StepLabel>
+                            </Step>
+                            <Step>
+                                <StepLabel>Contacted</StepLabel>
+                            </Step>
+                            <Step>
+                                <StepLabel>Interviewing</StepLabel>
+                            </Step>
+                            <Step>
+                                <StepLabel>Done</StepLabel>
+                            </Step>
+                        </Stepper>
                     </div>
                 </Paper>
 
