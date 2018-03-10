c693268a997240606e638f03aeb4f6f836463db7
Stephen Dorn <steved11@sbcglobal.net>
2018-03-10 13:03:34 -0600

    can dismiss candidates

--- src/components/childComponents/candidatePreview.js
+++ src/components/childComponents/candidatePreview.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
-import {Paper, Stepper, Step, StepLabel, StepButton} from 'material-ui';
+import {Paper, Stepper, Step, StepLabel, StepButton, FlatButton, RaisedButton} from 'material-ui';
 
 class PathwayPreview extends Component {
     constructor(props) {
@@ -8,32 +8,53 @@ class PathwayPreview extends Component {
 
         this.state = {
             step: 0,
+            dismissed: false,
         }
+    }
+
+    handleClick() {
+        this.setState({dismissed: !this.state.dismissed});
     }
 
     render() {
         return (
             <div className="candidatePreview">
-                <Paper className="candidatePreviewPaper candidatePreviewLi font16px font font14pxUnder700 font12pxUnder400" zDepth={2}>
+                <Paper
+                    className="candidatePreviewPaper candidatePreviewLi font16px font font14pxUnder700 font12pxUnder400"
+                    zDepth={2}>
                     <div className="candidatePreviewLiLeftContainer">{this.props.name}</div>
 
                     <div className="verticalDividerCandidatePreview"/>
 
                     <div className="candidatePreviewLiInfo" style={{display: 'inline-block'}}>
                         <Stepper activeStep={this.state.step}>
-                            <Step disabled={(0-this.state.step) > 1}>
+                            <Step disabled={((0 - this.state.step) > 1) || this.state.dismissed}>
                                 <StepButton onClick={() => this.setState({step: 0})}>Not Yet Contacted</StepButton>
                             </Step>
-                            <Step disabled={(1-this.state.step) > 1}>
+                            <Step disabled={((1 - this.state.step) > 1) || this.state.dismissed}>
                                 <StepButton onClick={() => this.setState({step: 1})}>Contacted</StepButton>
                             </Step>
-                            <Step disabled={(2-this.state.step) > 1}>
+                            <Step disabled={((2 - this.state.step) > 1) || this.state.dismissed}>
                                 <StepButton onClick={() => this.setState({step: 2})}>Interviewing</StepButton>
                             </Step>
-                            <Step disabled={(3-this.state.step) > 1}>
-                                <StepButton onClick={() => this.setState({step: 3})}>Done</StepButton>
+                            <Step disabled={((3 - this.state.step) > 1) || this.state.dismissed}>
+                                <StepButton onClick={() => this.setState({step: 3})}>Hired</StepButton>
                             </Step>
                         </Stepper>
+                        <div className="center">
+                            {this.state.dismissed ?
+                                <RaisedButton label="Dismissed"
+                                              primary={true}
+                                              labelStyle={{color:"white"}}
+                                              onClick={this.handleClick.bind(this)}
+                                />
+                                :
+                                <FlatButton label="Dismiss"
+                                            primary={true}
+                                            onClick={this.handleClick.bind(this)}
+                                />
+                            }
+                        </div>
                     </div>
                 </Paper>
 
