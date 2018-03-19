0744e31680568fed81eeaf946777bd0b8b842bf6
Austin Meyer <frizzkitten@gmail.com>
2018-03-19 13:27:26 -0500

    Revert "accepts png on frontend"
    This reverts commit 7ffd275cb79189e98132e9c78f5aba566620f38c.

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -498,13 +498,13 @@ class Profile extends Component {
                             paperClassName="dialogForSignup"
                             overlayClassName="dialogOverlay"
                         >
-                            {"Accepts .jpg and .png files under 2mb"}
+                            {"Accepts .jpg files under 2mb"}
                             <br/>
                             <input
                                 name="profilePicture"
                                 type="file"
                                 ref="profilePictureFile"
-                                accept="image/jpg,image/png"
+                                accept="image/jpg"
                                 onChange={this.handleImageChange.bind(this)}
                                 style={{marginTop:"20px"}}
                             />
