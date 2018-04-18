20902ad31190239254b06be4e26a3763e8c0e720
Stephen Dorn <steved11@sbcglobal.net>
2018-04-18 13:41:47 -0500

    button for uploading resume

--- src/components/pages/resumeScorer.js
+++ src/components/pages/resumeScorer.js
@@ -12,7 +12,25 @@ import HomepageTriangles from '../miscComponents/HomepageTriangles';
 import {browserHistory} from 'react-router';
 import MetaTags from 'react-meta-tags';
 
+const styles = {
+    horizList: {
+        position: "relative",
+        marginTop: "15px",
+        marginBottom: "25px"
+    },
+    horizListIcon: {
+        height: "50px",
+        marginBottom: "10px"
+        // position: "absolute",
+        // top: "0",
+        // bottom: "0",
+        // right: "80%",
+        // margin: "auto"
+    },
+};
+
 class ResumeScorer extends Component {
+
     render() {
         return (
             <div className="jsxWrapper noOverflowX">
@@ -34,6 +52,56 @@ class ResumeScorer extends Component {
                             {"Upload Your Resume"}
                         </button>
                     </div>
+                </div>
+                <div style={{marginTop: '60px', marginBottom: '40px', overflow: 'auto'}}>
+                    <div style={styles.horizList}>
+                        <div className="horizListFull">
+                            <div className="horizListSpacer" style={{marginLeft: "20%"}}
+                            >
+                                <div className="horizListText font18px font16pxUnder800 font12pxUnder700">
+                                    <img
+                                        alt="Puzzle Icon"
+                                        src="/icons/Key.png"
+                                        style={styles.horizListIcon}
+                                    /><br/>
+                                    Position and Industry <div className="above600only"><br/></div>Recommendations
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="horizListFull">
+                            <div className="horizListSpacer" style={{marginLeft: "5%", marginRight: '5%'}}>
+                                <div className="horizListText font18px font16pxUnder800 font12pxUnder700">
+                                    <img
+                                        alt="Lightbulb Icon"
+                                        src="/icons/Evaluate.png"
+                                        style={styles.horizListIcon}
+                                    /><br/>
+                                    Courses for Skills <div className="above600only"><br/></div>Training
+                                </div>
+                            </div>
+                        </div>
+                        <div className="horizListFull">
+                            <div className="horizListSpacer" style={{marginRight: "20%"}}>
+                                <div className="horizListText font18px font16pxUnder800 font12pxUnder700">
+                                    <img
+                                        alt="Trophy Icon"
+                                        src="/icons/Badge.png"
+                                        style={styles.horizListIcon}
+                                    /><br/>
+                                    Comparative Analysis <div className="above600only br"><br/></div>and Scoring
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="center" style={{marginBottom: '40px'}}>
+                    <button className="redToLightRedButtonExterior bigButton"
+                    >
+                        <div className="invertColorOnHover gradientBorderButtonInterior">
+                            Upload Your Resume
+                        </div>
+                    </button>
                 </div>
             </div>
         );
