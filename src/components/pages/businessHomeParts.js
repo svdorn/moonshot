ce1956a266a5a2898f148fcadfc462a3445c1285
Stephen Dorn <steved11@sbcglobal.net>
2018-05-20 12:48:40 -0500

    formatting and styling for first frame

--- src/components/pages/businessHomeParts.js
+++ src/components/pages/businessHomeParts.js
@@ -16,7 +16,7 @@ class BusinessHomeParts extends Component {
                     <div className="homepageTrajectory forBusiness" id="whatSkillsAreYouHiringFor">
                         <div className="homepageTrajectoryTextLeft forBusiness">
                             <div className="font18px font16pxUnder800 homepageTrajectoryTextLeftDiv forBusiness whiteText">
-                                <h2 className="pinkTextHome font28px font24pxUnder800 font22pxUnder500">What Skills Are You <div className="above800only br"><br/></div>Hiring For?</h2>
+                                <h2 className="pinkTextHome font28px font24pxUnder800 font22pxUnder500">Quickly identify which candidates <div className="above800only br"><br/></div>will be top performers</h2>
                                 Analyze candidates to see if they exhibit the profile of
                                 proven high performers in that position.
                             </div>
@@ -34,7 +34,7 @@ class BusinessHomeParts extends Component {
                     <div className="homepageTrajectory forBusiness">
                         <div className="homepageTrajectoryTextRight forBusiness">
                             <div className="font18px font16pxUnder800 homepageTrajectoryTextRightDiv forBusiness whiteText">
-                                <h2 className="blueTextHome font28px font24pxUnder800 font22pxUnder500">Course Pathways Curated <div className="above500only br"><br/></div>to the Skills You Need
+                                <h2 className="blueTextHome font28px font24pxUnder800 font22pxUnder500">Use data to eliminate biases <div className="above500only br"><br/></div>and guesswork
                                 </h2>
                                 Why read hundreds of resumes? Moonshot uses
                                 machine learning to reveal the empirical evidence
@@ -53,7 +53,7 @@ class BusinessHomeParts extends Component {
                     <div className="homepageTrajectory forBusiness" id="sponsorStudentsForBusiness">
                         <div className="homepageTrajectoryTextLeft forBusiness">
                             <div className="font18px font16pxUnder800 homepageTrajectoryTextLeftDiv forBusiness whiteText">
-                                <h2 className="orangeTextHome font28px font24pxUnder800 font22pxUnder500">Candidate Training</h2>
+                                <h2 className="orangeTextHome font28px font24pxUnder800 font22pxUnder500">Improve your candidate <div className="above800only br"><br/></div>experience</h2>
                                 83% of candidates rate their current experience as poor.
                                 Engage your candidates better so they can understand
                                 your company and how they fit.
