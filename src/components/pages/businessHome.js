610833a286908d70bc6925ece942a7ee60fc7e43
Austin Meyer <frizzkitten@gmail.com>
2018-05-21 14:29:24 -0500

    added three screenshots frame

--- src/components/pages/businessHome.js
+++ src/components/pages/businessHome.js
@@ -77,15 +77,6 @@ class BusinessHome extends Component {
                             <div className="skewedRectangle" />
                             <div className="skewedRectangle" />
                             <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
-                            <div className="skewedRectangle" />
                         </div>
                     </div>
                 </div>
@@ -96,8 +87,71 @@ class BusinessHome extends Component {
                     </div>
                 </div>
 
-                <div style={{marginTop:"400px"}}>
-                </div>
+                <section id="threeScreenshots">
+                    <div className="homepageTrajectory forBusiness">
+                        <div className="homepageTrajectoryTextLeft forBusiness">
+                            <div className="font18px font16pxUnder800 homepageTrajectoryTextLeftDiv forHome whiteText">
+                                <h2 className="pinkTextHome font28px font24pxUnder800 font22pxUnder500">Quickly identify which candidates <div className="above1000only br"><br/></div>will be top performers</h2>
+                                Analyze candidates to see if they exhibit the profile of
+                                proven high performers in that position.
+                            </div>
+                            <button className="slightlyRoundedButton marginTop10px pinkToPurpleButtonGradient whiteText">
+                                Hire Faster
+                            </button>
+                        </div>
+                        <div className="businessHomeTrajectoryImageRightNoBorder forBusiness">
+                            <img
+                                alt="My Candidates Management"
+                                src="/images/businessHome/ProductScreenshot3v6.png"
+                            />
+                            </div>
+                    </div>
+
+                    <br/>
+
+                    <div className="homepageTrajectory forBusiness">
+                        <div className="homepageTrajectoryTextRight forBusiness">
+                            <div className="font18px font16pxUnder800 homepageTrajectoryTextRightDiv forHome whiteText">
+                                <h2 className="blueTextHome font28px font24pxUnder800 font22pxUnder500">Use data to eliminate biases <div className="above900only br"><br/></div>and guesswork
+                                </h2>
+                                Why read hundreds of resumes? Moonshot uses
+                                machine learning to reveal the empirical evidence
+                                instead of conjecture based on a resume.
+                            </div>
+                            <button className="slightlyRoundedButton marginTop10px blueToPurpleButtonGradient whiteText">
+                                Hire Smarter
+                            </button>
+                        </div>
+                        <div className="businessHomeTrajectoryImagesLeft businessHomeTrajectoryImagesShadow forBusiness">
+                            <img
+                                alt="Predictive Insights"
+                                src="/images/businessHome/PredictiveInsights.jpg"
+                            />
+                        </div>
+                    </div>
+                    <br />
+
+                    <div className="homepageTrajectory forBusiness">
+                        <div className="homepageTrajectoryTextLeft forBusiness">
+                            <div className="font18px font16pxUnder800 homepageTrajectoryTextLeftDiv forHome whiteText">
+                                <h2 className="orangeTextHome font28px font24pxUnder800 font22pxUnder500">Improve your candidate <div className="above800only br"><br/></div>experience</h2>
+                                83% of candidates rate their current experience as poor.
+                                Engage your candidates better so they can understand
+                                your company and how they fit.
+                            </div>
+                            <button className="slightlyRoundedButton marginTop10px orangeToRedButtonGradient whiteText">
+                                Hire Better
+                            </button>
+                        </div>
+
+                        <div className="businessHomeTrajectoryImagesRight businessHomeTrajectoryImagesShadow forBusiness">
+                            <img
+                                alt="Analysis Text"
+                                src="/images/businessHome/ProductScreenshot5.jpg"
+                            />
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
