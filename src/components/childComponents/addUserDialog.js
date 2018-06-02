4c2e844d69ed6723f84fc7e8311edc041178f93f
Stephen Dorn <steved11@sbcglobal.net>
2018-06-02 11:24:48 -0500

    get position from database for first page

--- src/components/childComponents/addUserDialog.js
+++ src/components/childComponents/addUserDialog.js
@@ -3,9 +3,10 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {postEmployer} from '../../actions/usersActions';
-import {TextField, CircularProgress, RaisedButton, FlatButton, Dialog } from 'material-ui';
+import {TextField, CircularProgress, RaisedButton, FlatButton, Dialog, DropDownMenu, MenuItem, Divider } from 'material-ui';
 import {Field, reduxForm} from 'redux-form';
 import { browserHistory } from 'react-router';
+import axios from 'axios';
 
 const renderTextField = ({input, label, meta: {touched, error}, ...custom}) => (
     <TextField
@@ -42,11 +43,39 @@ class AddUserDialog extends Component {
         this.state = {
             emails: [],
             open: true,
+            screen: 1,
+            positions: [],
+            position: "",
+            // true if the business has no positions associated with it
+            noPositions: false,
         }
     }
 
     componentDidMount() {
-
+        let self = this;
+        axios.get("/api/business/positions", {
+            params: {
+                userId: this.props.currentUser._id,
+                verificationToken: this.props.currentUser.verificationToken
+            }
+        })
+        .then(function (res) {
+            let positions = res.data.positions;
+            console.log(positions)
+            if (Array.isArray(positions) && positions.length > 0) {
+                self.setState({
+                    positions,
+                    open: true,
+                    screen: 1
+                })
+            } else {
+                self.setState({
+                    noPositions: true,
+                    open: true,
+                    screen: 1
+                })
+            }
+        })
     }
 
     handleOpen = () => {
@@ -99,6 +128,10 @@ class AddUserDialog extends Component {
         })
     }
 
+    handlePositionChange = (event, index, positon) => {
+        this.setState({position})
+    };
+
     goTo(route) {
         // goes to the wanted page
         browserHistory.push(route);
@@ -109,6 +142,15 @@ class AddUserDialog extends Component {
 
     //name, email, password, confirm password, signup button
     render() {
+        const style = {
+            anchorOrigin: {
+                vertical: "top",
+                horizontal: "left"
+            },
+            menuLabelStyle: {
+                color: "rgba(255,255,255,.8)"
+            }
+        };
 
         const actions = [
             <FlatButton
@@ -118,58 +160,89 @@ class AddUserDialog extends Component {
             />,
         ];
 
+        // TODO get companies from DB
+        const positions = this.state.positions;
+        const positionItems = positions.map(function (position) {
+            return <MenuItem value={position.name} primaryText={position.name} key={position.name}/>
+        })
+
 
         return (
-            <Dialog
-                actions={actions}
-                modal={false}
-                open={this.state.open}
-                onRequestClose={this.handleClose}
-                autoScrollBodyContent={true}
-                paperClassName="dialogForBiz"
-                contentClassName="center"
-            >
-                <form onSubmit={this.handleSubmit.bind(this)} className="center">
-                    <div
-                        className="whiteTextImportant font28px font24pxUnder700 font20pxUnder500 marginTop10px">
-                        Predict Candidate Success
-                    </div>
-                    <Field
-                        name="name"
-                        component={renderTextField}
-                        label="Full Name*"
-                        style={{marginTop: '1px'}}
-                    /> <br/>
-                    <Field
-                        name="email"
-                        component={renderTextField}
-                        label="Email*"
-                    /><br/>
-                    <Field
-                        name="company"
-                        component={renderTextField}
-                        label="Company"
-                    /><br/>
-                    <Field
-                        name="phone"
-                        component={renderTextField}
-                        label="Phone Number"
-                    /><br/>
-                    <RaisedButton
-                        label="Send"
-                        type="submit"
-                        className="raisedButtonBusinessHome"
-                        style={{marginTop: '20px'}}
-                    />
-                    <br/>
-                    <div className="infoText i flex font12px whiteText center"
-                        style={{margin: '10px auto', width: '250px'}}>
-                        <div>Free for First Position</div>
-                        <div>•</div>
-                        <div>Unlimited Evaluations</div>
-                    </div>
-                </form>
-            </Dialog>
+            <div>
+            {this.state.screen === 1 ?
+                <Dialog
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                    autoScrollBodyContent={true}
+                    paperClassName="dialogForBiz"
+                    contentClassName="center"
+                >
+                <DropDownMenu value={this.state.position}
+                              onChange={this.handlePositionChange}
+                              labelStyle={style.menuLabelStyle}
+                              anchorOrigin={style.anchorOrigin}
+                              style={{fontSize: "20px", marginTop: "11px"}}
+                >
+                    <MenuItem value={""} primaryText="Position"/>
+                    <Divider/>
+                    {positionItems}
+                </DropDownMenu>
+                </Dialog>
+                :
+                <Dialog
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                    autoScrollBodyContent={true}
+                    paperClassName="dialogForBiz"
+                    contentClassName="center"
+                >
+                    <form onSubmit={this.handleSubmit.bind(this)} className="center">
+                        <div
+                            className="whiteTextImportant font28px font24pxUnder700 font20pxUnder500 marginTop10px">
+                            Predict Candidate Success
+                        </div>
+                        <Field
+                            name="name"
+                            component={renderTextField}
+                            label="Full Name*"
+                            style={{marginTop: '1px'}}
+                        /> <br/>
+                        <Field
+                            name="email"
+                            component={renderTextField}
+                            label="Email*"
+                        /><br/>
+                        <Field
+                            name="company"
+                            component={renderTextField}
+                            label="Company"
+                        /><br/>
+                        <Field
+                            name="phone"
+                            component={renderTextField}
+                            label="Phone Number"
+                        /><br/>
+                        <RaisedButton
+                            label="Send"
+                            type="submit"
+                            className="raisedButtonBusinessHome"
+                            style={{marginTop: '20px'}}
+                        />
+                        <br/>
+                        <div className="infoText i flex font12px whiteText center"
+                            style={{margin: '10px auto', width: '250px'}}>
+                            <div>Free for First Position</div>
+                            <div>•</div>
+                            <div>Unlimited Evaluations</div>
+                        </div>
+                    </form>
+                </Dialog>
+            }
+            </div>
         );
     }
 }
