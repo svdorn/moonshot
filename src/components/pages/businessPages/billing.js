9fec91c395e924b4900ba171246cf088cdb3df2e
Stephen Dorn <steved11@sbcglobal.net>
2018-06-15 13:10:00 -0500

    basic setup for billing page

---
+++ src/components/pages/businessPages/billing.js
@@ -0,0 +1,37 @@
+"use strict"
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {  } from '../../../actions/usersActions';
+
+
+class Billing extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        currentUser: state.users.currentUser
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+
+    }, dispatch);
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Billing);
