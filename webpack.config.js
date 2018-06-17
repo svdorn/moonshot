29e7b4da99dfaaf5f0d43db65695deda2801b8e3
Austin Meyer <frizzkitten@gmail.com>
2018-06-17 13:08:59 -0500

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,36 @@ var path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/client.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  plugins: [new webpack.IgnorePlugin(/credentials.js|node_modules/)],
-  watch: true,
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test:/\.js$/,
-        exclude:[
-            /node_modules/,
-            path.resolve(__dirname, "./credentials.js"),
-        ],
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
-      }
-    ]
-  }
+    entry: './src/client.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    plugins: [
+        new webpack.IgnorePlugin(/credentials.js|node_modules/)
+    ],
+    // optimization: {
+    //     minimizer: [
+    //         new UglifyJsPlugin({
+    //             cache: true
+    //         })
+    //     ]
+    // },
+    watch: true,
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude:[
+                    /node_modules/,
+                    path.resolve(__dirname, "./credentials.js"),
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            }
+        ]
+    }
 }
