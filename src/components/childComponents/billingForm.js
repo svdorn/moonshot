aa97575fe7bde1265532a90d5b6bb3d321f881ea
Stephen Dorn <steved11@sbcglobal.net>
2018-06-18 15:38:16 -0500

    billing back in

--- src/components/childComponents/billingForm.js
+++ src/components/childComponents/billingForm.js
@@ -28,13 +28,10 @@ class BillingForm extends Component {
         const userId = currentUser._id;
 
         this.props.stripe.createSource({type: 'card', owner: { name: currentUser.name}}).then(function(result) {
-            // Handle result.error or result.source
             if (result.error) {
                 console.log(result.error);
                 self.props.stopLoading();
                 self.props.addNotification("Error adding card, please review credit card information and retry.", "error");
-                console.log(result);
-                console.log("error");
             } else {
                 self.props.setupBillingCustomer(result.source, email, userId, verificationToken);
             }
