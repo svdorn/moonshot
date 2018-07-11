41f4039a8f0847c53388673620b42d7e3617b8c0
Stephen Dorn <steved11@sbcglobal.net>
2018-07-11 07:43:25 -0500

    emails work and updating email templates

--- apis/userApis.js
+++ apis/userApis.js
@@ -704,15 +704,20 @@ async function sendNotificationEmails(businessId, user) {
             moonshotUrl = 'http://localhost:8081/';
         }
 
+        const findById = { _id: businessId };
+
+        const business = await Businesses.findOne(findById).select("name");
+
         // send email to candidate
         let recipient = [user.email];
         let subject = "You've Finished Your Evaluation!";
         let content =
             '<div style="font-size:15px;text-align:center;font-family: Arial, sans-serif;color:#7d7d7d">'
-                + '<div style="font-size:28px;color:#0c0c0c;">Congratulations!</div>'
-                + '<p style="width:95%; display:inline-block; text-align:left;">Congratulations on finishing your evaluation! Your results have been sent to the employer. Sit tight and they will let you know the next steps moving forward.'
-                + '<br/><p style="width:95%; display:inline-block; text-align:left;">We&#39; glad to have you in the Moonshot process.!</p><br/>'
-                + '<p><b style="color:#0c0c0c">Questions?</b> Shoot an email to <b style="color:#0c0c0c">support@moonshotinsights.io</b></p>'
+                + '<p style="width:95%; display:inline-block; text-align:left;">Hi' + getFirstName(user.name) + ',</p>'
+                + '<p style="width:95%; display:inline-block; text-align:left;">My name is Justin and I am the Chief Product Officer at Moonshot Insights. I saw that you finished your evaluation for ' + business.name +
+                + '. I just wanted to let you know your results have been sent to the employer, just sit tight and we will let you know the next steps moving forward. I wish you the best of luck!</p><br/>'
+                + '<p style="width:95%; display:inline-block; text-align:left;">If you have any questions at all, please feel free to shoot me an email at <b style="color:#0c0c0c">Justin@MoonshotInsights.io</b> and I&#39;ll get back to you within 24 hours or I&#39;ll buy you a pizza!</p>'
+                + '<p style="width:95%; display:inline-block; text-align:left;">Sincerely,<br/><br/>Justin Ye<br/><i>Chief Product Officer</i><br/><b style="color:#0c0c0c">Justin@MoonshotInsights.io</b></p>'
                 + '<div style="background:#7d7d7d;height:2px;width:40%;margin:25px auto 25px;"></div>'
                 + '<a href="' + moonshotUrl + '" style="color:#00c3ff"><img alt="Moonshot Logo" style="height:100px;"src="https://image.ibb.co/kXQHso/Moonshot_Insights.png"/></a><br/>'
                 + '<div style="text-align:left;width:95%;display:inline-block;">'
@@ -736,7 +741,7 @@ async function sendNotificationEmails(businessId, user) {
             ]
         }
         try {
-            let users  = await Users.find(businessUserQuery).select("name email")
+            let users  = await Users.find(businessUserQuery).select("name email notifications")
             if (!users) {
                 return;
             }
@@ -745,7 +750,6 @@ async function sendNotificationEmails(businessId, user) {
             for (let i = 0; i < users.length; i++) {
                 recipient = users[i];
                 const notifications = users[i].notifications;
-                let numCandidates = 0;
                 if (notifications) {
                     let timeDiff = Math.abs(new Date() - notifications.lastSent);
                     switch(notifications.time) {
@@ -777,45 +781,56 @@ async function sendNotificationEmails(businessId, user) {
                     continue;
                 }
 
-
-                console.log("user: ", users[i]);
-                // send email to business
-                setTimeout(async function(recipient) {
-                        let reciever = [];
-                        reciever.push(recipient.email);
-                            let subject = numCandidates + ' Candidates Completed Your Evaluation';
-                            let content =
-                                '<div style="font-size:15px;text-align:center;font-family: Arial, sans-serif;color:#7d7d7d">'
-                                    + '<div style="font-size:28px;color:#0c0c0c;">Evaluation Update!</div>'
-                                    + '<p style="width:95%; display:inline-block; text-align:left;">' +numCandidates + ' candidates have completed your evaluation.'
-                                    + '<br/><p style="width:95%; display:inline-block; text-align:left;">See their results.</p><br/>'
-                                    + '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border-radius:14px 14px 14px 14px;color:white;padding:10px 5px 0px;text-decoration:none;margin:20px;background:#494b4d;" href="' + moonshotUrl + 'myCandidates'
-                                    + '">See Results</a>'
-                                    + '<p><b style="color:#0c0c0c">Questions?</b> Shoot an email to <b style="color:#0c0c0c">support@moonshotinsights.io</b></p>'
-                                    + '<div style="background:#7d7d7d;height:2px;width:40%;margin:25px auto 25px;"></div>'
-                                    + '<a href="' + moonshotUrl + '" style="color:#00c3ff"><img alt="Moonshot Logo" style="height:100px;"src="https://image.ibb.co/kXQHso/Moonshot_Insights.png"/></a><br/>'
-                                    + '<div style="text-align:left;width:95%;display:inline-block;">'
-                                        + '<div style="font-size:10px; text-align:center; color:#C8C8C8; margin-bottom:30px;">'
-                                        + '<i>Moonshot Learning, Inc.<br/><a href="" style="text-decoration:none;color:#D8D8D8;">1261 Meadow Sweet Dr<br/>Madison, WI 53719</a>.<br/>'
-                                        + '<a style="color:#C8C8C8; margin-top:20px;" href="' + moonshotUrl + 'unsubscribe>Opt-out of future messages.</a></i>'
-                                        + '</div>'
-                                    + '</div>'
-                                + '</div>';
-
-                            const sendFrom = "Moonshot";
-                            sendEmail(reviever, subject, content, sendFrom, undefined, function (success, msg) {
-                                console.log("success: ", success);
-                                console.log("msg" ,msg);
-                            })
-                        }
-                    , time);
+                sendDelayedEmail(recipient, time);
+                recipient = {};
+                time = 0;
             }
-            recipient = {};
         } catch (getUserError) {
             console.log("error getting user when agreeing to skill test terms: ", getUserError);
             return res.status(500).send("Error getting user.");
         }
     });
+}
+
+async function sendDelayedEmail(recipient, time) {
+    setTimeout(function() {
+        let moonshotUrl = 'https://www.moonshotinsights.io/';
+        // if we are in development, links are to localhost
+        if (!process.env.NODE_ENV) {
+            moonshotUrl = 'http://localhost:8081/';
+        }
+            let reciever = [];
+            console.log("in send delayed email: ", recipient);
+            console.log("time: ", time);
+            // get the number of candidates
+            let numCandidates = 0;
+            reciever.push(recipient.email);
+                let subject = numCandidates + ' Candidates Completed Your Evaluation';
+                let content =
+                    '<div style="font-size:15px;text-align:center;font-family: Arial, sans-serif;color:#7d7d7d">'
+                        + '<div style="font-size:28px;color:#0c0c0c;">Evaluation Update!</div>'
+                        + '<p style="width:95%; display:inline-block; text-align:left;">' +numCandidates + ' candidates have completed your evaluation.'
+                        + '<br/><p style="width:95%; display:inline-block; text-align:left;">See their results.</p><br/>'
+                        + '<a style="display:inline-block;height:28px;width:170px;font-size:18px;border-radius:14px 14px 14px 14px;color:white;padding:10px 5px 0px;text-decoration:none;margin:20px;background:#494b4d;" href="' + moonshotUrl + 'myCandidates'
+                        + '">See Results</a>'
+                        + '<p><b style="color:#0c0c0c">Questions?</b> Shoot an email to <b style="color:#0c0c0c">support@moonshotinsights.io</b></p>'
+                        + '<div style="background:#7d7d7d;height:2px;width:40%;margin:25px auto 25px;"></div>'
+                        + '<a href="' + moonshotUrl + '" style="color:#00c3ff"><img alt="Moonshot Logo" style="height:100px;"src="https://image.ibb.co/kXQHso/Moonshot_Insights.png"/></a><br/>'
+                        + '<div style="text-align:left;width:95%;display:inline-block;">'
+                            + '<div style="font-size:10px; text-align:center; color:#C8C8C8; margin-bottom:30px;">'
+                            + '<i>Moonshot Learning, Inc.<br/><a href="" style="text-decoration:none;color:#D8D8D8;">1261 Meadow Sweet Dr<br/>Madison, WI 53719</a>.<br/>'
+                            + '<a style="color:#C8C8C8; margin-top:20px;" href="' + moonshotUrl + 'unsubscribe>Opt-out of future messages.</a></i>'
+                            + '</div>'
+                        + '</div>'
+                    + '</div>';
+
+                const sendFrom = "Moonshot";
+                sendEmail(reciever, subject, content, sendFrom, undefined, function (success, msg) {
+                    console.log("success: ", success);
+                    console.log("msg" ,msg);
+                })
+            }
+        , time);
 }
 
 
