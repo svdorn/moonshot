a1064c77b903544fa090690409c217d80ce89b02
Austin Meyer <frizzkitten@gmail.com>
2018-07-25 22:15:57 -0500

    onboarding fixed progress bar follows you around and bugs you until you complete onboarding

--- src/components/miscComponents/fixedOnboardingProgress.js
+++ src/components/miscComponents/fixedOnboardingProgress.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {  } from '../../actions/usersActions';
 import OnboardingProgress from "./onboardingProgress";
+import { withRouter } from "react-router";
 
 
 class FixedOnboardingProgress extends Component {
@@ -13,15 +14,46 @@ class FixedOnboardingProgress extends Component {
         this.state = {};
     }
 
+    componentDidUpdate() {
+        console.log("updated");
+    }
+
     render() {
-        return (
-            <div className="fixed-onboarding-progress">
-                <div>
-                    <div className="center primary-white">{"Onboarding Progress"}</div>
-                    <OnboardingProgress className="fixed-progress-bar"/>
+        // console.log("rendering");
+        // get the current user
+        const user = this.props.currentUser;
+
+        // get the current path from the url
+        let pathname = undefined;
+        // try to get the path; lowercased because capitalization will vary
+        try { pathname = this.props.location.pathname.toLowerCase(); }
+        // if the pathname is not yet defined, don't do anything, this will be executed again later
+        catch (e) { pathname = ""; }
+        // if the user is in the process of onboarding and isn't on the onboarding page...
+
+        // if a bunch of conditions are true ...
+        if (user &&
+            // user must be an actual user
+            typeof user === "object" &&
+            // user has to have onboarding to see how far they are in onboarding
+            typeof user.onboarding === "object" &&
+            // only show the progress bar if the user isn't done
+            !user.onboarding.complete &&
+            // don't show the fixed progress bar while going through onboarding
+            pathname !== "/onboarding"
+        ) {
+            // ... show the onboarding progress bar in the lower right corner
+            return (
+                <div className="fixed-onboarding-progress">
+                    <div>
+                        <div className="center primary-white">{"Onboarding Progress"}</div>
+                        <OnboardingProgress className="fixed-progress-bar"/>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        // if those conditions aren't all true, don't return anything to render
+        else { return null; }
     }
 }
 
@@ -39,4 +71,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(FixedOnboardingProgress);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(FixedOnboardingProgress));
