bce8dc365e3ba6378c84011bf04ddab5a7b0a206
Austin Meyer <frizzkitten@gmail.com>
2018-08-05 19:05:02 -0500

    added boilerplate manual invite page

---
+++ src/components/pages/businessPages/onboarding/automateInvites/manualInvite.js
@@ -0,0 +1,38 @@
+"use strict"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {  } from "../../../../../actions/usersActions";
+import {  } from "../../../../../miscFunctions";
+
+
+class ManualInvite extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        currentUser: state.users.currentUser
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+
+    }, dispatch);
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManualInvite);
