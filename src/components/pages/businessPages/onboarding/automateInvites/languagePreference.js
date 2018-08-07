34342edad6a4d5ea910c0d4efeb28580a39ced7d
Austin Meyer <frizzkitten@gmail.com>
2018-08-07 17:09:33 -0500

    only lets you move on if you have entered text into a box or picked a non-text box

--- src/components/pages/businessPages/onboarding/automateInvites/languagePreference.js
+++ src/components/pages/businessPages/onboarding/automateInvites/languagePreference.js
@@ -98,6 +98,16 @@ class LanguagePreference extends Component {
         newArgs[`${type}Custom`] = value;
         this.setState(newArgs);
 
+        // if this input box is currently selected
+        if (this.state.selectedBox === `${type}Custom`) {
+            // see if the input is non-empty
+            const nextCallable = truthy(value);
+            // if redux state doesn't agree with our nextCallable, change it to match
+            if (this.props.automationStep.nextCallable !== nextCallable) {
+                this.props.changeAutomateInvites({ nextCallable });
+            }
+        }
+
         // // if there is a non-empty value in the text box and the button is not clickable ...
         // if (value && !this.props.automationStep.nextCallable) {
         //     // make the Next button clickable
