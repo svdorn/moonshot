5306223abd3ff524fae94f4d75f8b55e17341026
Austin Meyer <frizzkitten@gmail.com>
2018-08-09 10:06:28 -0500

    wording

--- src/components/pages/businessHome.js
+++ src/components/pages/businessHome.js
@@ -568,7 +568,7 @@ class BusinessHome extends Component {
                         <div>
                             <div className="center">
                                 <div className="font30px font26pxUnder850 font22pxUnder600 font20pxUnder400 center primary-cyan statisticsHeader">
-                                    Predictive Analytics Improve Hiring Results
+                                    Candidate Predictions Improve Hiring Results
                                 </div>
                                 <div style={{position:"relative"}}>
                                     <div className="flourishes3">
