967459cd5b06e9ed3bad252ec5d3dc4f7ebcec0d
Austin Meyer <frizzkitten@gmail.com>
2018-09-19 18:38:43 -0500

    removed unnecessary stuff

--- apis/evaluationApis.js
+++ apis/evaluationApis.js
@@ -15,8 +15,6 @@ const errors = require('./errors.js');
 // get helper functions
 const { sanitize,
         verifyUser,
-        sendEmail,
-        emailFooter,
         getAndVerifyUser,
         getFirstName,
         getUserFromReq,
