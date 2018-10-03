208909a5e28f6f8e6649a92aaffde26f6e018be2
Austin Meyer <frizzkitten@gmail.com>
2018-10-03 17:26:34 -0500

    removed dots from line graph

--- src/components/pages/dashboard/dashboardItems/candidates.js
+++ src/components/pages/dashboard/dashboardItems/candidates.js
@@ -164,7 +164,7 @@ class Candidates extends Component {
                 <LineChart style={chartStyle} width={250} height={120} data={this.state.graphData}>
                     <XAxis dataKey="ago" padding={{right:10,left:10}}/>
                     <YAxis />
-                    <Line type="monotone" dataKey="users" stroke={primaryCyan} />
+                    <Line type="monotone" dataKey="users" stroke={primaryCyan} dot={null}/>
                 </LineChart>
                 <div className="center" styleName="graph-x-label">{this.state.timeToGraph} Ago</div>
 
