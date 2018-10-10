459579292d1d4e941a8b1e9c2f772c40cf50035a
Austin Meyer <frizzkitten@gmail.com>
2018-10-09 22:48:37 -0400

    more carousel edits

--- src/components/pages/dashboard/dashboardItems/activity.js
+++ src/components/pages/dashboard/dashboardItems/activity.js
@@ -9,7 +9,7 @@ import Carousel from "../../../miscComponents/carousel";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import { primaryCyan } from "../../../../colors";
+import { primaryCyan, primaryWhite } from "../../../../colors";
 import axios from "axios";
 
 import "../dashboard.css";
@@ -207,6 +207,8 @@ class Activity extends Component {
                         frames={[frame1, frame2, frame3, frame4]}
                         transitionDuration={1000}
                         styleName="activity-carousel"
+                        color1={primaryWhite}
+                        color2={primaryCyan}
                     />
                 </div>
             </div>
