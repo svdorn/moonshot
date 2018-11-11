278720565506cb928b8921bb19a6049fc414be4e
Stephen Dorn <steved11@sbcglobal.net>
2018-11-11 09:29:02 -0800

    billing changes

--- apis/billingApis.js
+++ apis/billingApis.js
@@ -22,52 +22,57 @@ const billingApis = {
 }
 
 async function POST_customer(req, res) {
-    const email = sanitize(req.body.email);
-    const source = sanitize(req.body.source.id);
-    const userId = sanitize(req.body.userId);
-    const verificationToken = sanitize(req.body.verificationToken);
+    return new Promise(function(resolve, reject) {
+        const { email, source, userId, verificationToken } = sanitize(req.body);
 
-    // if one of the arguments doesn't exist, return with error code
-    if (!email || !source || !userId || !verificationToken) {
-        return res.status(400).send("Bad request.");
-    }
-
-    // send source to Stripe to create person
-    let customer;
-    try {
-        customer = await stripe.customers.create({
-            email,
-            source
-        });
-    } catch (createCustomerError) {
-        console.log("Error creting a customer: ", createCustomerError);
-        return res.status(403).send("Customer creation failed.");
-    }
-    // give business object the Stripe customer
-    let user;
-    let business;
-    try {
-        user = await getAndVerifyUser(userId, verificationToken);
-        business = await Businesses.findById(user.businessInfo.businessId);
-        if (!business) {
-            console.log("No business found with id: ", user.businessInfo.businessId);
-            throw "No business.";
+        // if one of the arguments doesn't exist, return with error code
+        if (!email || !source || !userId || !verificationToken) {
+            return res.status(400).send("Bad request.");
         }
-    } catch (getUserError) {
-        console.log("Error getting user or business from user: ", getUserError);
-        return res.status(403).send("You do not have permission to do add credit card.");
-    }
 
-    business.billingCustomerId = customer.id;
+        // send source to Stripe to create person
+        let customer;
+        try {
+            customer = await stripe.customers.create({
+                email,
+                source
+            });
+        } catch (createCustomerError) {
+            console.log("Error creting a customer: ", createCustomerError);
+            return res.status(403).send("Customer creation failed.");
+        }
+        // give business object the Stripe customer
+        let user;
+        let business;
+        try {
+            user = await getAndVerifyUser(userId, verificationToken);
+            business = await Businesses.findById(user.businessInfo.businessId);
+            if (!business) {
+                console.log("No business found with id: ", user.businessInfo.businessId);
+                throw "No business.";
+            }
+        } catch (getUserError) {
+            console.log("Error getting user or business from user: ", getUserError);
+            return res.status(403).send("You do not have permission to do add credit card.");
+        }
 
-    // save the business
-    try { await business.save(); }
-    catch (bizSaveError) {
-        console.log("Error saving business when adding credit card: ", bizSaveError);
-        return res.status(500).send("Server error, try again later.");
-    }
+        business.billingCustomerId = customer.id;
 
-    return res.json("Success");
+        // save the business
+        try { await business.save(); }
+        catch (bizSaveError) {
+            console.log("Error saving business when adding credit card: ", bizSaveError);
+            return res.status(500).send("Server error, try again later.");
+        }
+
+        return res.json("Success");
+    });
+}
+
+// TODO: METHOD DESCRIPTOR
+async function POST_customer(req, res) {
+    return new Promise(function(resolve, reject) {
+    })
 }
 
 module.exports = billingApis;
