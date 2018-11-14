03491666afa71c7fa9d5a60fe776b2c2ce55164f
Austin Meyer <frizzkitten@gmail.com>
2018-11-13 18:15:05 -0600

    factor graphs

--- src/components/pages/adminPages/dataDisplay/psych.js
+++ src/components/pages/adminPages/dataDisplay/psych.js
@@ -29,7 +29,8 @@ class Psych extends Component {
 
         this.state = {
             site: sites[0],
-            categoryIdx: 0
+            categoryIdx: 0,
+            comparableFactors: []
         };
     }
 
@@ -41,6 +42,26 @@ class Psych extends Component {
     // change the type of data we're getting (questions, facets, etc)
     handleCategoryChange = (event, categoryIdx) => {
         this.setState({ categoryIdx });
+    };
+
+    // mark a factor to compare
+    handleCheckMarkClick = factor => {
+        console.log("factor: ", factor);
+        let { comparableFactors } = this.state;
+        // go through each factor that's already clicked
+        for (let cfIdx = 0; cfIdx < comparableFactors.length; cfIdx++) {
+            // if it's the same as the one that was clicked ...
+            if (comparableFactors[cfIdx].name === factor.name) {
+                // ... remove it, then set the state to reflect the change
+                comparableFactors = comparableFactors
+                    .slice(0, cfIdx)
+                    .concat(comparableFactors.slice(cfIdx + 1));
+                return this.setState({ comparableFactors });
+            }
+        }
+        // if the factor didn't exists within the array, add it, then set state
+        comparableFactors.push(factor);
+        this.setState({ comparableFactors });
     };
 
     // the tabs that show the different sites you can choose from
@@ -64,12 +85,98 @@ class Psych extends Component {
     }
 
     // the display for factor data
-    factors() {
-        return "factors";
-    }
+    // the display for facet data
+    factors = () => {
+        const self = this;
+
+        const data = [
+            { name: "-4.75", uv: 300, pv: 456 },
+            { name: "-4.25", uv: -145, pv: 230 },
+            { name: "-3.75", uv: -100, pv: 345 },
+            { name: "-3.25", uv: -8, pv: 450 },
+            { name: "-2.75", uv: 100, pv: 321 },
+            { name: "-2.25", uv: 9, pv: 235 },
+            { name: "-1.75", uv: 53, pv: 267 },
+            { name: "-1.25", uv: 252, pv: 378 },
+            { name: "-0.75", uv: 79, pv: 210 },
+            { name: "-0.25", uv: 294, pv: 23 },
+            { name: "0.25", uv: 43, pv: 45 },
+            { name: "0.75", uv: -74, pv: 90 },
+            { name: "1.25", uv: -71, pv: 130 },
+            { name: "1.75", uv: -117, pv: 11 },
+            { name: "2.25", uv: -186, pv: 107 },
+            { name: "2.75", uv: -16, pv: 926 },
+            { name: "3.25", uv: -125, pv: 653 },
+            { name: "3.75", uv: 222, pv: 366 },
+            { name: "4.25", uv: 372, pv: 486 },
+            { name: "4.75", uv: 182, pv: 512 }
+        ];
+
+        const factors = [
+            {
+                name: "Honesty-Humility",
+                dataPoints: data,
+                average: 2.3,
+                stdDev: 0.41
+            },
+            { name: "Extraversion", dataPoints: data, average: 2.3, stdDev: 0.41 },
+            {
+                name: "Dinglification",
+                dataPoints: data,
+                average: -1.6,
+                stdDev: 0.53
+            }
+        ];
+
+        const factorGraphs = factors.map(factor => {
+            return [
+                <div styleName="facet-graph" key={factor.name}>
+                    <div>
+                        <div>{factor.name}</div>
+                        <br />
+                        <BarChart
+                            style={{ display: "inline-block" }}
+                            width={600}
+                            height={300}
+                            data={factor.dataPoints}
+                            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <ReferenceLine y={0} stroke="#000" />
+                            <Brush dataKey="name" height={30} stroke={colors.primaryCyan} />
+                            <Bar dataKey="pv" fill={colors.primaryCyan} />
+                        </BarChart>
+                    </div>
+                    <div>
+                        <div>Average: {factor.average}</div>
+                        <div>Std. dev.: {factor.stdDev}</div>
+                    </div>
+                    <div
+                        className="checkbox smallCheckbox whiteCheckbox"
+                        onClick={() => self.handleCheckMarkClick(factor)}
+                    >
+                        <img
+                            alt=""
+                            className={
+                                "checkMark" +
+                                self.state.comparableFactors.some(cf => cf.name === factor.name)
+                            }
+                            src={"/icons/CheckMarkRoundedWhite" + self.props.png}
+                        />
+                    </div>
+                </div>,
+                <br key={`${factor.name} br`} />
+            ];
+        });
+
+        return factorGraphs;
+    };
 
     // the display for facet data
-    facets() {
+    facets = () => {
         const data = [
             { name: "-4.75", uv: 300, pv: 456 },
             { name: "-4.25", uv: -145, pv: 230 },
@@ -131,15 +238,15 @@ class Psych extends Component {
         });
 
         return facetLis;
-    }
+    };
 
     // the display for question data
-    questions() {
+    questions = () => {
         return "questions";
-    }
+    };
 
     // the display for output data
-    outputs() {
+    outputs = () => {
         const outputs = [
             {
                 text:
@@ -185,7 +292,7 @@ class Psych extends Component {
             </li>
         );
         return <ul>{outputItems}</ul>;
-    }
+    };
 
     render() {
         const { categoryIdx } = this.state;
@@ -193,7 +300,7 @@ class Psych extends Component {
         console.log("categoryIdx: ", categoryIdx);
 
         return (
-            <div>
+            <div style={{ textAlign: "center" }}>
                 {this.siteSelector()}
                 {this.categorySelector()}
                 {categoryDisplays[categoryIdx]()}
@@ -204,7 +311,8 @@ class Psych extends Component {
 
 function mapStateToProps(state) {
     return {
-        currentUser: state.users.currentUser
+        currentUser: state.users.currentUser,
+        png: state.users.png
     };
 }
 
