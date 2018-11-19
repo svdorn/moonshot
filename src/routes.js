ddec28c6b9237f9e350cc8619c68b599f88ea495
Austin Meyer <frizzkitten@gmail.com>
2018-11-18 23:09:11 -0600

    removed Results.js

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,6 @@ import Signup from "./components/pages/signup";
 import MyCandidates from "./components/pages/businessPages/myCandidates";
 import MyEmployees from "./components/pages/businessPages/myEmployees";
 import MyEvaluations from "./components/pages/myEvaluations/myEvaluations";
-import Results from "./components/pages/businessPages/results";
 import EmployeeResults from "./components/pages/businessPages/employeeResults";
 import Billing from "./components/pages/businessPages/billing";
 
