69f16ccaecd826772403cac2922e2622c6dc880a
Austin Meyer <frizzkitten@gmail.com>
2018-11-18 21:33:49 -0600

    carousel.js removed currentUser (not used)

--- src/components/miscComponents/carousel.js
+++ src/components/miscComponents/carousel.js
@@ -1,11 +1,10 @@
-"use strict"
+"use strict";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import {  } from "../../actions/usersActions";
-import {  } from "../../miscFunctions";
+import {} from "../../actions/usersActions";
+import {} from "../../miscFunctions";
 import { primaryWhite } from "../../colors";
-
 
 class Carousel extends Component {
     constructor(props) {
@@ -21,16 +20,16 @@ class Carousel extends Component {
         };
     }
 
-
     previousFrame() {
         // if not on the first frame, return the previous frame in the array
         if (this.state.frameIndex > 0) {
             return this.props.frames[this.state.frameIndex - 1];
         }
         // if on the first frame, return the last frame in the array
-        else { return this.props.frames[this.props.frames.length - 1]; }
+        else {
+            return this.props.frames[this.props.frames.length - 1];
+        }
     }
-
 
     nextFrame() {
         // if on the last frame, return the first frame
@@ -38,31 +37,40 @@ class Carousel extends Component {
             return this.props.frames[0];
         }
         // otherwise return the next frame
-        else { return this.props.frames[this.state.frameIndex + 1]; }
+        else {
+            return this.props.frames[this.state.frameIndex + 1];
+        }
     }
-
 
     content() {
         let animationStyle = {};
         if (this.state.animationClass) {
             // default is .5 second animation
-            const millis = typeof this.props.transitionDuration === "number" ? this.props.transitionDuration : 500;
+            const millis =
+                typeof this.props.transitionDuration === "number"
+                    ? this.props.transitionDuration
+                    : 500;
             const transition = `all ${millis / 1000}s ease`;
             animationStyle = {
                 WebkitTransition: transition,
                 MsTransition: transition,
                 transition: transition
-            }
+            };
         }
         return (
             <div className={`content${this.state.animationClass}`}>
-                <div key="previous" style={animationStyle}>{ this.previousFrame() }</div>
-                <div key="current" style={animationStyle}>{ this.props.frames[this.state.frameIndex] }</div>
-                <div key="next" style={animationStyle}>{ this.nextFrame() }</div>
+                <div key="previous" style={animationStyle}>
+                    {this.previousFrame()}
+                </div>
+                <div key="current" style={animationStyle}>
+                    {this.props.frames[this.state.frameIndex]}
+                </div>
+                <div key="next" style={animationStyle}>
+                    {this.nextFrame()}
+                </div>
             </div>
         );
     }
-
 
     bottomCircles(color1, color2) {
         const selectedStyle = { background: `linear-gradient(to top, ${color1}, ${color2})` };
@@ -78,15 +86,12 @@ class Carousel extends Component {
                 <div
                     key={`circle${frameIndex}`}
                     className="frame-position-circle"
-                    style={selected ? {...style, ...selectedStyle} : style}
+                    style={selected ? { ...style, ...selectedStyle } : style}
                 />
             );
         }
-        return (
-            <div key="bottomCircles">{ circles }</div>
-        )
+        return <div key="bottomCircles">{circles}</div>;
     }
-
 
     move(direction) {
         // only allow the user to move around if no animations are in-progress
@@ -110,20 +115,26 @@ class Carousel extends Component {
                 }
             }
             // the class to add so that the objects inside the container slide around
-            const animationClass = ` animate-${direction}`
+            const animationClass = ` animate-${direction}`;
             // how long the animation lasts
-            const transitionDuration = typeof this.props.transitionDuration === "number" ? this.props.transitionDuration : 500;
+            const transitionDuration =
+                typeof this.props.transitionDuration === "number"
+                    ? this.props.transitionDuration
+                    : 500;
             // set the animation class, don't let the user move around until animation is done
             this.setState({ animationClass, canNavigate: false }, () => {
                 // then wait for the animation to be done (.5 secs)
                 setTimeout(() => {
                     // then set the current frame and get rid of the animation
-                    this.setState({ frameIndex: newFrameIndex, animationClass: "", canNavigate: true });
+                    this.setState({
+                        frameIndex: newFrameIndex,
+                        animationClass: "",
+                        canNavigate: true
+                    });
                 }, transitionDuration);
             });
         }
     }
-
 
     render() {
         // if no content is given, can't show anything
@@ -145,35 +156,34 @@ class Carousel extends Component {
 
         return (
             <div className={"carousel " + className} styleName={styleName} style={style}>
-                <div style={{position: "relative"}}>
-                    { this.content() }
+                <div style={{ position: "relative" }}>
+                    {this.content()}
                     <div
-                        className="left circleArrowIcon" style={arrowStyle}
+                        className="left circleArrowIcon"
+                        style={arrowStyle}
                         onClick={() => this.move("back")}
                     />
                     <div
-                        className="right circleArrowIcon" style={arrowStyle}
+                        className="right circleArrowIcon"
+                        style={arrowStyle}
                         onClick={() => this.move("next")}
                     />
                 </div>
-                { this.bottomCircles(color1, color2) }
+                {this.bottomCircles(color1, color2)}
             </div>
         );
     }
 }
 
-
 function mapStateToProps(state) {
-    return {
-        currentUser: state.users.currentUser
-    };
+    return {};
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-
-    }, dispatch);
+    return bindActionCreators({}, dispatch);
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Carousel);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Carousel);
