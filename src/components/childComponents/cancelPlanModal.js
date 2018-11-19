4a6bb714ce5634f256150a2b20898f336cc9e32f
Austin Meyer <frizzkitten@gmail.com>
2018-11-18 20:36:14 -0600

    cancelPlanModel.js currentUser checks

--- src/components/childComponents/cancelPlanModal.js
+++ src/components/childComponents/cancelPlanModal.js
@@ -1,18 +1,24 @@
-"use strict"
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { addNotification, cancelBillingPlan, generalAction, pauseBillingPlan, updateStore } from '../../actions/usersActions';
-import Button from '@material-ui/core/Button';
+"use strict";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+    addNotification,
+    cancelBillingPlan,
+    generalAction,
+    pauseBillingPlan,
+    updateStore
+} from "../../actions/usersActions";
+import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import CornersButton from '../miscComponents/cornersButton';
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import CornersButton from "../miscComponents/cornersButton";
 import colors from "../../colors";
 import { goTo } from "../../miscFunctions";
 import { button } from "../../classes.js";
-import axios from 'axios';
+import axios from "axios";
 
 import "./cancelPlanModal.css";
 
@@ -30,15 +36,22 @@ class CancelPlanModal extends Component {
         this.props.generalAction("CLOSE_CANCEL_PLAN_MODAL");
         this.props.updateStore("blurMenu", false);
         this.setState({ frame: "First", error: undefined });
-    }
+    };
 
-    changeFrame = (frame) => {
-        this.setState({ frame, error: undefined })
-    }
+    changeFrame = frame => {
+        this.setState({ frame, error: undefined });
+    };
 
     pausePlan = () => {
         const pause = document.getElementById("pause");
         const message = pause.value;
+
+        if (!this.props.currentUser) {
+            return this.props.addNotification(
+                "You aren't logged in! Try refreshing the page.",
+                "error"
+            );
+        }
 
         const { _id, verificationToken } = this.props.currentUser;
 
@@ -48,11 +61,19 @@ class CancelPlanModal extends Component {
         }
         this.props.pauseBillingPlan(_id, verificationToken, message);
         this.setState({ frame: "First" });
-    }
+    };
 
     cancelPlan = () => {
         const cancel = document.getElementById("cancel");
         const message = cancel.value;
+
+        // make sure there's a user logged in
+        if (!this.props.currentUser) {
+            return this.props.addNotification(
+                "You aren't logged in! Try refreshing the page.",
+                "error"
+            );
+        }
 
         const { _id, verificationToken } = this.props.currentUser;
 
@@ -62,35 +83,38 @@ class CancelPlanModal extends Component {
         }
         this.props.cancelBillingPlan(_id, verificationToken, message);
         this.setState({ frame: "Cancel Confirmation" });
-    }
+    };
 
     cancelPlanNoMessage = () => {
+        // make sure there's a user logged in
+        if (!this.props.currentUser) {
+            return this.props.addNotification(
+                "You aren't logged in! Try refreshing the page.",
+                "error"
+            );
+        }
+
         const { _id, verificationToken } = this.props.currentUser;
 
         this.props.cancelBillingPlan(_id, verificationToken, null);
         this.setState({ frame: "Cancel Confirmation" });
-    }
+    };
 
     firstFrame() {
         return (
             <div styleName="first-frame">
-                <div className="font22px font20pxUnder700 font16pxUnder500">
-                    Cancel Plan
-                </div>
+                <div className="font22px font20pxUnder700 font16pxUnder500">Cancel Plan</div>
                 <div styleName="header-seperator" />
                 <div className="font18px font16pxUnder700 font14pxUnder500">
-                    Would you pause the plan <div className="br above800Only"><br/></div>if you had the option?
+                    Would you pause the plan{" "}
+                    <div className="br above800Only">
+                        <br />
+                    </div>if you had the option?
                 </div>
                 <div className="font18px font16pxUnder700 font14pxUnder500">
-                    <div onClick={() => this.changeFrame("Pause")}>
-                        Yes
-                    </div>
-                    <div>
-                        |
-                    </div>
-                    <div onClick={() => this.changeFrame("Cancel")}>
-                        No
-                    </div>
+                    <div onClick={() => this.changeFrame("Pause")}>Yes</div>
+                    <div>|</div>
+                    <div onClick={() => this.changeFrame("Cancel")}>No</div>
                 </div>
             </div>
         );
@@ -100,12 +124,9 @@ class CancelPlanModal extends Component {
         return (
             <div styleName="pause-plan">
                 How long do you need to pause the plan?
-                {this.state.error ?
-                    <div className="font14px secondary-red">
-                        {this.state.error}
-                    </div>
-                    : null
-                }
+                {this.state.error ? (
+                    <div className="font14px secondary-red">{this.state.error}</div>
+                ) : null}
                 <div>
                     <textarea styleName="textarea" id="pause" placeholder="Type info here..." />
                 </div>
@@ -128,12 +149,9 @@ class CancelPlanModal extends Component {
         return (
             <div styleName="cancel-plan">
                 {"What's the single biggest reason for you cancelling?"}
-                {this.state.error ?
-                    <div className="font14px secondary-red">
-                        {this.state.error}
-                    </div>
-                    : null
-                }
+                {this.state.error ? (
+                    <div className="font14px secondary-red">{this.state.error}</div>
+                ) : null}
                 <div>
                     <textarea styleName="textarea" id="cancel" placeholder="Type here..." />
                 </div>
@@ -157,7 +175,13 @@ class CancelPlanModal extends Component {
                 </div>
                 <div styleName="header-seperator" />
                 <div className="font18px font16pxUnder700 font14pxUnder500 marginTop15px">
-                    You{"'"}ll continue to have access <div className="br above800Only"><br/></div>until your current plan ends. All of <div className="br above800Only"><br/></div>your data will be saved for you.
+                    You{"'"}ll continue to have access{" "}
+                    <div className="br above800Only">
+                        <br />
+                    </div>until your current plan ends. All of{" "}
+                    <div className="br above800Only">
+                        <br />
+                    </div>your data will be saved for you.
                 </div>
             </div>
         );
@@ -167,7 +191,7 @@ class CancelPlanModal extends Component {
         const { frame } = this.state;
 
         let content = null;
-        switch(frame) {
+        switch (frame) {
             case "First":
                 content = this.firstFrame();
                 break;
@@ -186,23 +210,16 @@ class CancelPlanModal extends Component {
         }
 
         return (
-            <Dialog
-                open={!!this.props.open}
-                maxWidth={false}
-                onClose={this.close}
-            >
+            <Dialog open={!!this.props.open} maxWidth={false} onClose={this.close}>
                 <div styleName="modal-container">
-                    <DialogContent styleName="modal">
-                        { content }
-                    </DialogContent>
-                    {frame === "First" || frame === "Cancel Confirmation" ?
+                    <DialogContent styleName="modal">{content}</DialogContent>
+                    {frame === "First" || frame === "Cancel Confirmation" ? (
                         <DialogActions>
                             <Button onClick={this.close} color="inherit">
                                 Close
                             </Button>
                         </DialogActions>
-                        : null
-                    }
+                    ) : null}
                 </div>
             </Dialog>
         );
@@ -210,13 +227,16 @@ class CancelPlanModal extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        addNotification,
-        cancelBillingPlan,
-        generalAction,
-        pauseBillingPlan,
-        updateStore
-    }, dispatch);
+    return bindActionCreators(
+        {
+            addNotification,
+            cancelBillingPlan,
+            generalAction,
+            pauseBillingPlan,
+            updateStore
+        },
+        dispatch
+    );
 }
 
 function mapStateToProps(state) {
@@ -227,4 +247,7 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CancelPlanModal);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CancelPlanModal);
