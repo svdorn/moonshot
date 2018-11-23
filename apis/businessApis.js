fc70de109e93cf2c153bd89e53d3050b501e14ae
Austin Meyer <frizzkitten@gmail.com>
2018-11-23 16:58:05 -0600

    working on persistent user session for initial biz users

--- apis/businessApis.js
+++ apis/businessApis.js
@@ -77,7 +77,6 @@ const businessApis = {
 
 const { sendIntercomPlanUpdate } = require("./evaluationApis");
 
-
 // ----->> START APIS <<----- //
 
 async function GET_adminList(req, res) {
@@ -320,27 +319,25 @@ async function POST_createBusinessAndUser(req, res) {
         user.hmac = hash;
     }
 
+    // save the user
     try {
         user = await user.save();
     } catch (saveUserError) {
-        console.log(
-            "Error saving user with biz info while signing up from business signup: ",
-            saveUserError
-        );
+        console.log("Error saving new account admin: ", saveUserError);
         return res.status(500).send(errors.SERVER_ERROR);
     }
 
+    // save the user's session - assume they want to stay logged in
+    try {
+        saveSession(req.session, user);
+    } catch (e) {
+        console.log("Error saving new account admin's session: ", e);
+    }
+
+    console.log("req.session: ", req.session);
+
     // return successfully to user
     res.status(200).send({ user: frontEndUser(user), fullAccess: true });
-
-    // save the session so that the user stays logged in
-    req.session.userId = user._id;
-    req.session.verificationToken = user.verificationToken;
-    req.session.save(function(err) {
-        if (err) {
-            console.log("ERROR SAVING SESSION OF NEW ACCOUNT ADMIN: ", err);
-        }
-    });
 
     // send email to everyone if there's a new sign up
     // do this after sending success message to user just in case this fails
@@ -374,6 +371,40 @@ async function POST_createBusinessAndUser(req, res) {
     } catch (alertEmailError) {
         console.log("Error sending alert email about new user: ", alertEmailError);
     }
+}
+
+// save the user's id and verification token in the session to keep them logged in
+async function saveSession(session, user, token) {
+    return new Promise(function(resolve, reject) {
+        if (!session) {
+            return reject(new Error("No session object provided to saveSession."));
+        }
+
+        // get userId and verificationToken
+        if (typeof user === "object") {
+            var { _id: userId, verificationToken } = user;
+        } else {
+            var userId = user;
+            var verificationToken = token;
+        }
+
+        if (!userId || !verificationToken) {
+            return reject(
+                new Error(`Invalid userId: ${userId} or verificationToken: ${verificationToken}`)
+            );
+        }
+
+        // save the session so that the user stays logged in
+        session.userId = userId;
+        session.verificationToken = verificationToken;
+        session.save(function(err) {
+            if (err) {
+                return reject(err);
+            } else {
+                return resolve();
+            }
+        });
+    });
 }
 
 // creates a new ACCOUNT ADMIN user
@@ -1744,31 +1775,40 @@ async function POST_changeHiringStage(req, res) {
         if (
             business &&
             business.fullAccess &&
-            (!business.billing || (business.billing && !business.billing.subscription && !business.billing.customPlan)) &&
+            (!business.billing ||
+                (business.billing &&
+                    !business.billing.subscription &&
+                    !business.billing.customPlan)) &&
             (business.candidateCount && business.candidateCount < 20)
         ) {
             business.fullAccess = false;
 
             // get all account admins for this business
-            try { var admins = await Users.find({ "userType": "accountAdmin", "businessInfo.businessId": mongoose.Types.ObjectId(business._id) }).select("intercom"); }
-            catch(getUsersError) {
-                console.log("error getting admins when trying to send them free trial ending message");
+            try {
+                var admins = await Users.find({
+                    userType: "accountAdmin",
+                    "businessInfo.businessId": mongoose.Types.ObjectId(business._id)
+                }).select("intercom");
+            } catch (getUsersError) {
+                console.log(
+                    "error getting admins when trying to send them free trial ending message"
+                );
             }
             // will contain all the promises for sending emails
             let intercomPromises = [];
 
             // add a promise to create a code and send an email for each given address
             admins.forEach(admin => {
-                intercomPromises.push(
-                    sendIntercomPlanUpdate(admin.intercom, "ended")
-                );
+                intercomPromises.push(sendIntercomPlanUpdate(admin.intercom, "ended"));
             });
 
             // wait for all the emails to send
             try {
                 await Promise.all(intercomPromises);
             } catch (sendEmailsError) {
-                console.log("error getting admins when trying to send them free trial ending message");
+                console.log(
+                    "error getting admins when trying to send them free trial ending message"
+                );
             }
         }
 
