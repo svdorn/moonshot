a344b0fed7358600204d22f1fb05af91f35136b4
Stephen Dorn <steved11@sbcglobal.net>
2018-12-14 11:57:36 -0600

    loader to delete

--- src/components/childComponents/deleteEvalModal.js
+++ src/components/childComponents/deleteEvalModal.js
@@ -44,6 +44,9 @@ class DeleteEvalModal extends Component {
             businessId: currentUser.businessInfo.businessId,
             positionId: positionId
         };
+
+        this.props.generalAction("START_LOADING");
+
         // delete in backend
         axios
             .post("/api/business/deleteEvaluation", credentials)
@@ -51,8 +54,10 @@ class DeleteEvalModal extends Component {
                 self.props.setDeletedEvaluation(res.data.deletedId);
                 // update the page that it was on to have the deleted position
                 self.setState({ frame: "Second" });
+                self.props.generalAction("STOP_LOADING");
             })
             .catch(err => {
+                self.props.generalAction("STOP_LOADING");
                 console.log("error getting positions: ", err);
                 if (err.response && err.response.data) {
                     console.log(err.response.data);
@@ -69,11 +74,14 @@ class DeleteEvalModal extends Component {
                     All of the data you{"'"}ve collected on this position will still be saved for
                     you. Do you wish to continue and delete the {this.props.name} position?
                 </div>
-                <div className="font18px font16pxUnder700 font14pxUnder500">
-                    <div onClick={this.deleteEval}>Yes</div>
-                    <div>|</div>
-                    <div onClick={this.close}>No</div>
-                </div>
+                {this.props.loading ? <CircularProgress />
+                    :
+                    <div className="font18px font16pxUnder700 font14pxUnder500">
+                        <div onClick={this.deleteEval}>Yes</div>
+                        <div>|</div>
+                        <div onClick={this.close}>No</div>
+                    </div>
+                }
             </div>
         );
     }
@@ -138,6 +146,7 @@ function mapStateToProps(state) {
         png: state.users.png,
         open: state.users.deleteEvalModal,
         name: state.users.deleteEvalName,
+        loading: state.users.loadingSomething,
         positionId: state.users.deleteEvalId
     };
 }
